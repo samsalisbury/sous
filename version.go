c9fc6098254967d6aca631fe6a0e325cf6a1b328
Sam Salisbury <samsalisbury@gmail.com>
2016-04-04 13:13:54 +0100

    Build script + version from flags

--- version.go
+++ version.go
@@ -1,16 +1,19 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/samsalisbury/semv"
-)
-
-const version = "1.0.0-alpha"
+import "github.com/samsalisbury/semv"
 
 var (
+	// These variables are set by build flags.
+	VersionString, OS, Arch, GoVersion string
 	// Revision should be set by the build process using build flags.
 	Revision string
 	// Version is the current version of Sous
-	Version = semv.MustParse(fmt.Sprintf("%s+%s", version, Revision))
+	Version semv.Version
 )
+
+func init() {
+	if VersionString == "" {
+		VersionString = "0.0.0-unversioned"
+	}
+	Version = semv.MustParse(VersionString)
+}
