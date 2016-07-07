63ea11911b7d72b41e02144514f5c5bc66413ccd
Sam Salisbury <samsalisbury@gmail.com>
2016-07-07 16:43:35 +0100

    Remove unnecessary docker ref in DummyRegistry

--- ext/singularity/dummy_registry.go
+++ ext/singularity/dummy_registry.go
@@ -1,9 +1,6 @@
 package singularity
 
-import (
-	"github.com/opentable/sous/ext/docker"
-	"github.com/opentable/sous/lib"
-)
+import "github.com/opentable/sous/lib"
 
 // DummyNameCache implements the Builder interface by returning a
 // computed image name for a given source version
@@ -17,7 +14,7 @@ func NewDummyRegistry() *DummyRegistry {
 
 // TODO: Factor out name cache concept from core sous lib & get rid of this func.
 func (dc *DummyRegistry) GetArtifact(sv sous.SourceVersion) (*sous.BuildArtifact, error) {
-	return docker.DockerBuildArtifact(sv.String()), nil
+	return &sous.BuildArtifact{Name: sv.String(), Type: "dummy"}, nil
 }
 
 // GetSourceVersion implements part of ImageMapper
