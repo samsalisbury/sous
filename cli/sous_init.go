96b91da1d8ae20aa36eb08af8c03f934cea6abc1
Sam Salisbury <samsalisbury@gmail.com>
2016-07-14 18:14:06 +0100

    WIP: sous init for offline mode

--- cli/sous_init.go
+++ cli/sous_init.go
@@ -1,16 +1,25 @@
 package cli
 
 import (
-	sous "github.com/opentable/sous/lib"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/opentable/sous/lib"
 	"github.com/opentable/sous/util/cmdr"
+	"github.com/opentable/sous/util/firsterr"
 	"github.com/samsalisbury/semv"
 )
 
 // SousInit is the command description for `sous init`
 type SousInit struct {
+	Sous          *Sous
 	User          LocalUser
 	Config        LocalSousConfig
 	SourceContext *sous.SourceContext
+	flags         struct {
+		RepoURL, RepoOffset string
+	}
 }
 
 func init() { TopLevelCommands["init"] = &SousInit{} }
@@ -28,6 +37,13 @@ flesh out some additional details.
 // Help returns the help string for this command
 func (si *SousInit) Help() string { return sousInitHelp }
 
+func (si *SousInit) AddFlags(fs *flag.FlagSet) {
+	fs.StringVar(&si.flags.RepoURL, "repo-url", "",
+		"the source code repo for this project (e.g. github.com/user/project)")
+	fs.StringVar(&si.flags.RepoOffset, "repo-offset", "",
+		"the subdir within the repo where the source code lives (empty for root)")
+}
+
 // Execute fulfills the cmdr.Executor interface
 func (si *SousInit) Execute(args []string) cmdr.Result {
 	c := si.SourceContext
@@ -35,13 +51,22 @@ func (si *SousInit) Execute(args []string) cmdr.Result {
 	if err != nil {
 		v = semv.MustParse("0.0.0-unversioned+" + c.Revision)
 	}
+
+	var repoURL, repoOffset string
+	if err := firsterr.Parallel().Set(
+		func(e *error) { repoURL, *e = si.ResolveRepoURL() },
+		func(e *error) { repoOffset, *e = si.ResolveRepoOffset() },
+	); err != nil {
+		return EnsureErrorResult(err)
+	}
+
 	m := sous.Manifest{
 		Source: sous.SourceLocation{
-			RepoURL:    sous.RepoURL(c.PossiblePrimaryRemoteURL),
-			RepoOffset: sous.RepoOffset(c.OffsetDir),
+			RepoURL:    sous.RepoURL(repoURL),
+			RepoOffset: sous.RepoOffset(repoOffset),
 		},
-		Deployments: map[string]sous.PartialDeploySpec{
-			"Global": sous.PartialDeploySpec{
+		Deployments: sous.DeploySpecs{
+			"Global": {
 				DeployConfig: sous.DeployConfig{
 					Resources:    sous.Resources{},
 					Env:          map[string]string{},
@@ -52,4 +77,31 @@ func (si *SousInit) Execute(args []string) cmdr.Result {
 		},
 	}
 	return SuccessYAML(m)
+}
+
+func (si *SousInit) ResolveRepoURL() (string, error) {
+	repoURL := si.flags.RepoURL
+	if repoURL == "" {
+		repoURL = si.SourceContext.PossiblePrimaryRemoteURL
+		if repoURL == "" {
+			return "", fmt.Errorf("no repo URL found, please use -repo-url")
+		}
+		sous.Log.Info.Printf("using repo URL %q (from git remotes)", repoURL)
+	}
+	if !strings.HasPrefix(repoURL, "github.com/") {
+		return "", fmt.Errorf("repo URL must begin with github.com/")
+	}
+	return repoURL, nil
+}
+
+func (si *SousInit) ResolveRepoOffset() (string, error) {
+	repoOffset := si.flags.RepoOffset
+	if repoOffset == "" {
+		repoOffset := si.SourceContext.OffsetDir
+		sous.Log.Info.Printf("using current workdir repo offset: %q", repoOffset)
+	}
+	if repoOffset[:1] == "/" {
+		return "", fmt.Errorf("repo offset cannot begin with /, it is relative")
+	}
+	return repoOffset, nil
 }
