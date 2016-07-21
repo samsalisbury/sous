698c0fb9d0bff2f186cc788c5a080dd3ce6381e4
Sam Salisbury <samsalisbury@gmail.com>
2016-07-21 16:07:46 +0100

    ext/docker: config env vars renamed

--- ext/docker/config.go
+++ ext/docker/config.go
@@ -4,10 +4,10 @@ type Config struct {
 	RegistryHost string `env:"SOUS_DOCKER_REGISTRY_HOST"`
 	// DatabaseDriver is the name of the driver to use for local
 	// persistence.
-	DatabaseDriver string `env:"SOUS_DB_DRIVER"`
+	DatabaseDriver string `env:"SOUD_DOCKER_DB_DRIVER"`
 	// DatabaseConnection is the database connection string for local
 	// persistence.
-	DatabaseConnection string `env:"SOUS_DB_CONN"`
+	DatabaseConnection string `env:"SOUS_DOCKER_DB_CONN"`
 }
 
 // DefaultConfig builds a default configuration, which can be then overridden by
