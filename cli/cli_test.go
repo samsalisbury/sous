f707a63c9a8e0e7710b1c0d31205a1f5a1d33797
Judson <nyarly@gmail.com>
2016-08-09 16:31:52 -0700

    Building tests around rectify

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/samsalisbury/semv"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInvokeRectifyWithDebugFlags(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
 
@@ -19,4 +21,15 @@ func TestInvokeRectifyWithDebugFlags(t *testing.T) {
 	exe, err := c.Prepare([]string{`sous`, `rectify`, `-d`, `-v`, `-all`})
 	assert.NoError(err)
 	assert.Len(exe.Args, 0)
+	require.IsType(&SousRectify{}, exe.Cmd)
+
+	rect := exe.Cmd.(*SousRectify)
+
+	assert.NotNil(rect.Config)
+	assert.NotNil(rect.DockerClient)
+	assert.NotNil(rect.Deployer)
+	assert.NotNil(rect.Registry)
+	assert.NotNil(rect.GDM)
+	require.NotNil(rect.flags)
+	assert.Equal(rect.flags.all, true)
 }
