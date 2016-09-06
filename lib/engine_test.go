9c5037bf8a32a29dd1ba9d05c329088ab61d652d
Sam Salisbury <samsalisbury@gmail.com>
2016-09-06 17:27:33 +0100

    Remove circular reference.

--- lib/engine_test.go
+++ lib/engine_test.go
@@ -2,8 +2,6 @@ package sous
 
 import (
 	"testing"
-
-	"github.com/opentable/sous/ext/github"
 )
 
 func TestEngine_ParseSourceLocation_noHosts(t *testing.T) {
@@ -13,7 +11,7 @@ func TestEngine_ParseSourceLocation_noHosts(t *testing.T) {
 	if (sl != SourceLocation{}) {
 		t.Errorf("got non-zero source location: %#v", sl)
 	}
-	if err == nil {
+	if actualErr == nil {
 		t.Fatalf("got nil; want error %q", expected)
 	}
 	actual := actualErr.Error()
@@ -22,25 +20,11 @@ func TestEngine_ParseSourceLocation_noHosts(t *testing.T) {
 	}
 }
 
-func TestEngine_ParseSourceLocation_hitGenericHost(t *testing.T) {
-	e := &Engine{
-		SourceHosts: []SourceHost{github.SourceHost{}, GenericHost{}},
-	}
-	expected := SourceLocation{Host: "hello"}
-	actual, err := e.ParseSourceLocation("hello")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if actual != expected {
-		t.Errorf("got:\n%#v; want:\n%#v", actual, expected)
-	}
-}
-
 func TestEngine_ParseSourceLocation_genericHost(t *testing.T) {
 	e := &Engine{
 		SourceHosts: []SourceHost{GenericHost{}},
 	}
-	expected := SourceLocation{Host: "hello"}
+	expected := SourceLocation{Repo: "hello"}
 	actual, err := e.ParseSourceLocation("hello")
 	if err != nil {
 		t.Fatal(err)
