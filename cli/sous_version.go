d229d615af1920270396ec943648fb436c7a8618
Sam Salisbury <samsalisbury@gmail.com>
2016-09-07 14:42:54 +0100

    Tidy up cli/sous_version comments and help.

--- cli/sous_version.go
+++ cli/sous_version.go
@@ -2,6 +2,7 @@ package cli
 
 import "github.com/opentable/sous/util/cmdr"
 
+// SousVersion is the 'sous version' command.
 type SousVersion struct {
 	Sous *Sous
 }
@@ -13,15 +14,12 @@ print the version of sous
 
 prints the current version of sous. Please include the output from this
 command with any bug reports sent to https://github.com/opentable/sous/issues
-
-args:
-
-Sous is versioned using semver. There are three versioned pieces of Sous:
-Sous Engine, Sous Server, and Sous CLI.
 `
 
+// Help returns help for sous version.
 func (*SousVersion) Help() string { return sousVersionHelp }
 
+// Execute runs the 'sous version' command.
 func (sv *SousVersion) Execute(args []string) cmdr.Result {
 	return cmdr.Successf("sous version %s", sv.Sous.Version)
 }
