6f66cd4baf1fc2bbfad1a744126d4e402b908be8
Judson <nyarly@gmail.com>
2016-09-19 17:48:00 -0700

    Starting to form up

--- server/handle_manifest.go
+++ server/handle_manifest.go
@@ -21,18 +21,38 @@ type (
 /*
 To recap:
 
-To look up a manifest, ma
+To look up a manifest, we need a manifestID:
+ManifestID{
+	SourceLocation{
+		Repo
+		Offset
+	}
+	Flavor
+}
+Which is a SourceLocation
 */
+
+func manifestIDFromValues(qv *QueryValues) (ManifestID, error) {
+	repo := qv.Get("repo")
+	offset := qv.Get("offset")
+	flavor := qv.Get("flavor")
+}
 
 // Exchange implements Exchanger
 func (gmh *GETManifestHandler) Exchange() (interface{}, int) {
-	repo := gmh.Get("repo")
-	offset := gmh.Get("offset")
+	mid, err := manifestIDFromValues(gmh.QueryValues)
+	if err != nil {
+		return nil, http.StatusNotFound
+	}
+	m, err := gmh.State.Manifests.Get(mid)
+	if err != nil {
+		return nil, http.StatusNotFound
+	}
+	return m, http.StatusOK
 }
 
 // Exchange implements Exchanger
 func (pmh *PUTManifestHandler) Exchange() (interface{}, int) {
-	repo := gmh.Get("repo")
-	offset := gmh.Get("offset")
+	mid, err := manifestIDFromValues(pmh.QueryValues)
 
 }
