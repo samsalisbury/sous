22c5f862533b3bbf36b5979c8e4c4866d5d5bc63
Sam Salisbury <samsalisbury@gmail.com>
2016-10-10 16:42:15 +0100

    config: test EnsureDirExists

--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/user"
 	"path"
@@ -37,5 +39,43 @@ func TestDefaultStateLocation(t *testing.T) {
 		if actual != expected {
 			t.Errorf("%d: %q got %q; want %q", i, tc.XDGDataHome, actual, expected)
 		}
+	}
+}
+
+func TestEnsureDirExists(t *testing.T) {
+	testDataDir := "testdata/gen"
+	if err := os.RemoveAll(testDataDir); err != nil {
+		t.Fatal(err)
+	}
+	extantDir := path.Join(testDataDir, "test-dir")
+	if err := os.MkdirAll(extantDir, 0777); err != nil {
+		t.Fatal(err)
+	}
+	if err := EnsureDirExists(extantDir); err != nil {
+		t.Error(err)
+	}
+	nonExistentDir := path.Join(testDataDir, "other-dir")
+	if err := EnsureDirExists(nonExistentDir); err != nil {
+		t.Error(err)
+	}
+	s, err := os.Stat(nonExistentDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !s.IsDir() {
+		t.Errorf("created path %q is not a directory", nonExistentDir)
+	}
+	filePath := path.Join(testDataDir, "a-file")
+	if err := ioutil.WriteFile(filePath, []byte("x"), 0777); err != nil {
+		t.Fatal(err)
+	}
+	expectedErr := fmt.Sprintf("%q exists and is not a directory", filePath)
+	err = EnsureDirExists(filePath)
+	if err == nil {
+		t.Errorf("got nil error; want %q", expectedErr)
+	}
+	actualErr := err.Error()
+	if actualErr != expectedErr {
+		t.Errorf("got error %q; want %q", actualErr, expectedErr)
 	}
 }
