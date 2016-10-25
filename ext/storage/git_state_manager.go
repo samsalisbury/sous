72935ad57909a7552c4581fe7d511762034daaa3
Sam Salisbury <samsalisbury@gmail.com>
2016-10-25 13:40:44 +0100

    More meaningful log for git error.

--- ext/storage/git_state_manager.go
+++ ext/storage/git_state_manager.go
@@ -38,7 +38,11 @@ func (gsm *GitStateManager) git(cmd ...string) error {
 	git.Dir = gsm.DiskStateManager.BaseDir
 	//git.Env = []string{"GIT_CONFIG_NOSYSTEM=true", "HOME=none", "XDG_CONFIG_HOME=none"}
 	out, err := git.CombinedOutput()
-	sous.Log.Debug.Printf("%v: exit %s", git.Args, err)
+	if err == nil {
+		sous.Log.Debug.Printf("%+v: success", git.Args)
+	} else {
+		sous.Log.Debug.Printf("%+v: error: %v", git.Args, err)
+	}
 	sous.Log.Vomit.Print("git: " + string(out))
 	return errors.Wrapf(err, strings.Join(git.Args, " ")+": "+string(out))
 }
