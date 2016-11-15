e69b46cbba8e514d13e453632c8ebc381e15a6c3
Sam Salisbury <samsalisbury@gmail.com>
2016-11-15 16:13:42 +0000

    Manifest: adds Validate test cases.

--- lib/manifest_test.go
+++ lib/manifest_test.go
@@ -3,6 +3,8 @@ package sous
 import (
 	"fmt"
 	"testing"
+
+	"github.com/samsalisbury/semv"
 )
 
 var manifestTests = []struct {
@@ -19,6 +21,69 @@ var manifestTests = []struct {
 		FlawDesc:         `ManifestKind "some invalid kind" not valid`,
 		RepairError:      "unable to repair invalid ManifestKind",
 	},
+	{
+		OriginalManifest: &Manifest{
+			Kind: ManifestKindService,
+			Deployments: DeploySpecs{
+				"some-cluster": DeploySpec{
+					DeployConfig: DeployConfig{
+						Resources: Resources{
+							"cpus": "1",
+							// NOTE: Missing memory.
+							"ports": "1",
+						},
+						NumInstances: 3,
+					},
+					Version: semv.MustParse("1"),
+				},
+			},
+		},
+		FixedManifest: &Manifest{
+			Kind: ManifestKindService,
+			Deployments: DeploySpecs{
+				"some-cluster": DeploySpec{
+					DeployConfig: DeployConfig{
+						Resources: Resources{
+							"cpus": "1",
+							// NOTE: Memory repaired by setting to default.
+							"memory": "100",
+							"ports":  "1",
+						},
+						NumInstances: 3,
+					},
+					Version: semv.MustParse("1"),
+				},
+			},
+		},
+		FlawDesc: "Missing resource field: memory",
+	},
+	{
+		// NOTE: This one is valid, hence no FlawDesc.
+		OriginalManifest: &Manifest{
+			Kind: ManifestKindService,
+			Deployments: DeploySpecs{
+				"Global": DeploySpec{
+					DeployConfig: DeployConfig{
+						// NOTE: These resources are inherited.
+						Resources: Resources{
+							"cpus":   "1",
+							"memory": "256",
+							"ports":  "1",
+						},
+					},
+				},
+				"some-cluster": DeploySpec{
+					DeployConfig: DeployConfig{
+						Resources: Resources{
+						// NOTE: Empty; inherited from Global.
+						},
+						NumInstances: 3,
+					},
+					Version: semv.MustParse("1"),
+				},
+			},
+		},
+	},
 }
 
 func TestManifest_Validate(t *testing.T) {
@@ -27,6 +92,9 @@ func TestManifest_Validate(t *testing.T) {
 		flaws := m.Validate()
 		expectedNumFlaws := 1
 		if len(flaws) != expectedNumFlaws {
+			for _, f := range flaws {
+				t.Error(f)
+			}
 			t.Fatalf("got %d flaws; want %d", len(flaws), expectedNumFlaws)
 		}
 		if test.FlawDesc != "" {
@@ -37,13 +105,15 @@ func TestManifest_Validate(t *testing.T) {
 			}
 		}
 		err := flaws[0].Repair()
+		expected := test.RepairError
 		if test.RepairError == "" {
 			if err != nil {
 				t.Fatal(err)
 			}
+		} else if err == nil {
+			t.Fatalf("got nil; want error %q", expected)
 		} else {
 			actual := err.Error()
-			expected := test.RepairError
 			if actual != expected {
 				t.Errorf("got error %q; want %q", actual, expected)
 			}
