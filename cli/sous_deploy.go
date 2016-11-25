7acbd60de3bf0dff2bd86923343efb6dce1699f8
Sam Salisbury <samsalisbury@gmail.com>
2016-11-25 15:56:29 +0000

    cli: SousDeploy does not rectify if Server set.

--- cli/sous_deploy.go
+++ cli/sous_deploy.go
@@ -10,6 +10,7 @@ import (
 
 // SousDeploy is the command description for `sous deploy`.
 type SousDeploy struct {
+	Config            graph.LocalSousConfig
 	CLI               *CLI
 	DeployFilterFlags config.DeployFilterFlags
 	OTPLFlags         config.OTPLFlags
@@ -56,13 +57,18 @@ func (sd *SousDeploy) Execute(args []string) cmdr.Result {
 		DeployFilterFlags: sd.DeployFilterFlags,
 		OTPLFlags:         sd.OTPLFlags,
 	}, []string{})
+
+	sd.CLI.OutputResult(res)
 	if !sd.CLI.IsSuccess(res) {
 		return res
 	}
-	sd.CLI.OutputResult(res)
 
+	if sd.Config.Server != "" {
+		return Successf("Deployment instigated; handed off to server.")
+	}
+
+	// Running serverless, so run rectify.
 	rect := &SousRectify{SourceFlags: sd.DeployFilterFlags}
 	rect.flags.dryrun = sd.rectifyFlags.dryrun
-
 	return sd.CLI.Plumbing(rect, []string{})
 }
