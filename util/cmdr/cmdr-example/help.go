2b45cda28afce3ed78fc83b4732953004989a4ed
Lars Lehtonen <lars.lehtonen@gmail.com>
2016-12-05 17:18:44 -0800

    Removed more dead code from cmdr-example.

--- util/cmdr/cmdr-example/help.go
+++ util/cmdr/cmdr-example/help.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"github.com/opentable/sous/util/cmdr"
 )
 
@@ -16,32 +14,6 @@ func (c helpCommand) Execute(subcommands []string) cmdr.Result {
 		return cmdr.EnsureErrorResult(err)
 	}
 	return cmdr.Successf(help)
-}
-
-func (c helpCommand) AlternateExecute(args []string) cmdr.Result {
-	b := bytes.Buffer{}
-	b.WriteString("\n")
-	b.WriteString("cmdr-example hello world\n\n")
-	if len(args) > 0 {
-		// the subcommand is the first string in args
-		cName := args[0]
-		b.WriteString(cName + "\n\n")
-		// check that the Command struct exists within the map of
-		// Command structs.
-		cmd, ok := cmds[cName]
-		if !ok {
-			// There wasn't a Command struct representing the requested
-			// command stored in the map of Command structs, so we're
-			// letting the user know that there isn't any help available.
-			b.WriteString(fmt.Sprintf("No help for command %s.", cName))
-		} else {
-			b.WriteString(cmd.Help())
-		}
-	} else {
-		b.WriteString("subcommands:\n\n")
-		b.WriteString(subTable(root.Subcommands()))
-	}
-	return cmdr.Successf(b.String())
 }
 
 func (c helpCommand) Help() string {
