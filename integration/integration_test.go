e5b80bc926f7bcd3b762d1a3e7887829e25c8682
Sam Salisbury <samsalisbury@gmail.com>
2016-12-14 14:53:51 +0000

    integration: Explicitly run cache assertions twice.
    - Previously we had duplicated tests which happened to fail
      when the cache failed to return the same result twice, due
      to deficiencies in the testing framework.
    - Now these tests will continue to fail after we correct the
      framework.

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -63,54 +63,24 @@ func TestGetRunningDeploymentSet_testCluster(t *testing.T) {
 
 	clusters := []string{"test-cluster"}
 
-	ds, which := deploymentWithRepo(clusters, nc, assert, d, "github.com/opentable/docker-grafana")
-	deps := ds.Snapshot()
-	if assert.Equal(3, len(deps)) {
-		grafana := deps[which]
-		assert.Equal(SingularityURL, grafana.Cluster.BaseURL)
-		assert.Regexp("^0\\.1", grafana.Resources["cpus"])    // XXX strings and floats...
-		assert.Regexp("^100\\.", grafana.Resources["memory"]) // XXX strings and floats...
-		assert.Equal("1", grafana.Resources["ports"])         // XXX strings and floats...
-		assert.Equal(17, grafana.SourceID.Version.Patch)
-		assert.Equal("91495f1b1630084e301241100ecf2e775f6b672c", grafana.SourceID.Version.Meta)
-		assert.Equal(1, grafana.NumInstances)
-		assert.Equal(sous.ManifestKindService, grafana.Kind)
-	}
+	// We run this test more than once to check that cache behaviour is
+	// consistent whether the cache is already warmed up or not.
+	const numberOfTestRuns = 2
 
-	ResetSingularity()
-}
-
-// TODO: do the double-run in a single test rather than rely on deficiencies in our testing infra
-func TestGetRunningDeploymentSet_testCluster2(t *testing.T) {
-	//sous.Log.Vomit.SetFlags(sous.Log.Vomit.Flags() | log.Ltime)
-	//sous.Log.Vomit.SetOutput(os.Stderr)
-	//sous.Log.Vomit.Print("Starting stderr output")
-	sous.Log.Debug.SetFlags(sous.Log.Debug.Flags() | log.Ltime)
-	sous.Log.Debug.SetOutput(os.Stderr)
-	sous.Log.Debug.Print("Starting stderr output")
-	assert := assert.New(t)
-
-	registerLabelledContainers()
-	drc := docker_registry.NewClient()
-	drc.BecomeFoolishlyTrusting()
-	nc := docker.NewNameCache("", drc, newInMemoryDB("grds"))
-	client := singularity.NewRectiAgent()
-	d := singularity.NewDeployer(client)
-
-	clusters := []string{"test-cluster"}
-
-	ds, which := deploymentWithRepo(clusters, nc, assert, d, "github.com/opentable/docker-grafana")
-	deps := ds.Snapshot()
-	if assert.Equal(3, len(deps)) {
-		grafana := deps[which]
-		assert.Equal(SingularityURL, grafana.Cluster.BaseURL)
-		assert.Regexp("^0\\.1", grafana.Resources["cpus"])    // XXX strings and floats...
-		assert.Regexp("^100\\.", grafana.Resources["memory"]) // XXX strings and floats...
-		assert.Equal("1", grafana.Resources["ports"])         // XXX strings and floats...
-		assert.Equal(17, grafana.SourceID.Version.Patch)
-		assert.Equal("91495f1b1630084e301241100ecf2e775f6b672c", grafana.SourceID.Version.Meta)
-		assert.Equal(1, grafana.NumInstances)
-		assert.Equal(sous.ManifestKindService, grafana.Kind)
+	for i := 0; i < numberOfTestRuns; i++ {
+		ds, which := deploymentWithRepo(clusters, nc, assert, d, "github.com/opentable/docker-grafana")
+		deps := ds.Snapshot()
+		if assert.Equal(3, len(deps)) {
+			grafana := deps[which]
+			assert.Equal(SingularityURL, grafana.Cluster.BaseURL)
+			assert.Regexp("^0\\.1", grafana.Resources["cpus"])    // XXX strings and floats...
+			assert.Regexp("^100\\.", grafana.Resources["memory"]) // XXX strings and floats...
+			assert.Equal("1", grafana.Resources["ports"])         // XXX strings and floats...
+			assert.Equal(17, grafana.SourceID.Version.Patch)
+			assert.Equal("91495f1b1630084e301241100ecf2e775f6b672c", grafana.SourceID.Version.Meta)
+			assert.Equal(1, grafana.NumInstances)
+			assert.Equal(sous.ManifestKindService, grafana.Kind)
+		}
 	}
 
 	ResetSingularity()
