a1ef7edcf7c5e03f6405bb4bc05344fa2abd2847
Judson Lester <jlester@opentable.com>
2016-12-27 13:28:37 -0800

    Actually including the endpoint now

--- server/handle_status.go
+++ server/handle_status.go
@@ -3,15 +3,20 @@ package server
 import (
 	"net/http"
 
+	"github.com/opentable/sous/graph"
 	"github.com/opentable/sous/lib"
 )
 
 type (
 	// StatusResource encapsulates a status response.
-	StatusResource struct{}
+	StatusResource struct {
+		Deployments           []*sous.Deployment
+		Completed, InProgress *sous.ResolveStatus
+	}
 
 	// StatusHandler handles requests for status.
 	StatusHandler struct {
+		GDM          graph.CurrentGDM
 		AutoResolver *sous.AutoResolver
 	}
 )
@@ -21,6 +26,10 @@ func (*StatusResource) Get() Exchanger { return &StatusHandler{} }
 
 // Exchange implements the Handler interface.
 func (h *StatusHandler) Exchange() (interface{}, int) {
-	status := h.AutoResolver.Status()
+	status := StatusResource{Deployments: []*sous.Deployment{}}
+	for _, d := range h.GDM.Snapshot() {
+		status.Deployments = append(status.Deployments, d)
+	}
+	status.Completed, status.InProgress = h.AutoResolver.Statuses()
 	return status, http.StatusOK
 }
