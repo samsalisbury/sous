fd0e4277da6f257ae5f0087824989cc407f8d883
Lars Lehtonen <lars.lehtonen@gmail.com>
2017-01-05 15:31:40 -0800

    cmdr TestFindBottomCmd()

--- util/cmdr/help_test.go
+++ util/cmdr/help_test.go
@@ -32,3 +32,27 @@ func TestHelp(t *testing.T) {
 		}
 	}
 }
+
+func TestFindBottomCmd(t *testing.T) {
+	// TestFindBottomCmd relies on &TestCommandWithSubcommands having
+	// subcommands that are plain commands that do not satisfy
+	// the Subcommander interface.
+	c := &TestCommandWithSubcommands{}
+
+	bcp := findBottomCommand(c, []string{"test", "nonCommandArg"})
+	bc := *bcp
+	if _, ok := bc.(Subcommander); ok {
+		t.Fatal("The bottom command was not expected to have subcommands")
+	} else {
+		t.Log("The bottom command correctly has no subcommands.")
+	}
+
+	bscp := findBottomCommand(c, []string{"not-command"})
+	bsc := *bscp
+	if _, ok := bsc.(Subcommander); ok {
+		t.Log("The provided argument is not a subcommand, thus the top level command is also the bottom command")
+	} else {
+		t.Fatal("The provided argument is not a subcommand, so the bottom command should have been the top command.")
+	}
+
+}
