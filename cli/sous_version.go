7b6b58d6e47044a213b9fb140e208eec0eac62d1
Lars Lehtonen <lars.lehtonen@gmail.com>
2017-01-10 10:52:28 -0800

    Default version for local builds is 0.0.0-devbuild
    sous version prints warning on dev versions and location of release binaries.

--- cli/sous_version.go
+++ cli/sous_version.go
@@ -1,6 +1,9 @@
 package cli
 
-import "github.com/opentable/sous/util/cmdr"
+import (
+	"github.com/opentable/sous/util/cmdr"
+	"github.com/samsalisbury/semv"
+)
 
 // SousVersion is the 'sous version' command.
 type SousVersion struct {
@@ -20,7 +23,17 @@ func (*SousVersion) Help() string { return sousVersionHelp }
 
 // Execute runs the 'sous version' command.
 func (sv *SousVersion) Execute(args []string) cmdr.Result {
-	out := `sous version %s (%s %s/%s)`
+	out := `sous version %s (%s %s/%s)%s`
+
 	s := sv.Sous
-	return cmdr.Successf(out, s.Version, s.GoVersion, s.OS, s.Arch)
+	minVer := semv.Version{
+		Major: 0,
+		Minor: 0,
+		Patch: 1,
+	}
+	var warning string
+	if s.Version.Less(minVer) {
+		warning = "\n\nDevelopment version. Download supported releases from https://github.com/opentable/sous\n"
+	}
+	return cmdr.Successf(out, s.Version, s.GoVersion, s.OS, s.Arch, warning)
 }
--- version.go
+++ version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // VersionString is the version of Sous.
-const VersionString = "0.1.1-beta.1"
+const VersionString = "0.0.0-devbuild"
 
 var (
 	// Version is the version of Sous.
