520dd6d1be9c9225b6b384dde15bde00e525a872
Judson Lester <jlester@opentable.com>
2017-01-18 17:48:18 -0800

    Expanding coverage on IsTransientResolveError

--- lib/resolve_errors_test.go
+++ lib/resolve_errors_test.go
@@ -12,6 +12,8 @@ func TestIsTransientResolveError(t *testing.T) {
 	assert := assert.New(t)
 
 	assert.False(IsTransientResolveError(fmt.Errorf("Hi!")))
+	assert.False(IsTransientResolveError(&UnacceptableAdvisory{}))
+	assert.False(IsTransientResolveError(errors.Wrap(&MissingImageNameError{}, "wrapped")))
 	assert.True(IsTransientResolveError(&CreateError{}))
 	assert.True(IsTransientResolveError(errors.Wrap(&CreateError{}, "even if wrapped")))
 }
