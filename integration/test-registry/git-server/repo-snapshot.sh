c1adbe8040686b4ce0969df83a32fae9d30ce967
Judson Lester <jlester@opentable.com>
2017-01-25 15:41:25 -0800

    Added sous-server and sous-demo repos

--- integration/test-registry/git-server/repo-snapshot.sh
+++ integration/test-registry/git-server/repo-snapshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-ssh -F ./ssh-config -i ./git_pubkey_rsa -p 2222 root@192.168.99.100 "cd /; tar zvc repos > repos.tgz"
+ssh -F ./ssh-config -i ./git_pubkey_rsa -p 2222 root@192.168.99.100 "cd /; tar zc repos > repos.tgz"
 scp -F ./ssh-config -i ./git_pubkey_rsa -P 2222 root@192.168.99.100:/repos.tgz .
