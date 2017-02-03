04a6023b0708d4888139c9f6077de86ba07efc98
Sam Salisbury <samsalisbury@gmail.com>
2017-02-03 10:53:59 +0000

    server: Read separate user name/email HTTP headers.

--- server/user.go
+++ server/user.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/opentable/sous/lib"
 )
@@ -10,17 +9,11 @@ import (
 // ClientUser is the sous.User configured in the calling client.
 type ClientUser sous.User
 
-// parseHeaders parses a Headers from the headers of a HTTP request.
+// getUser parses a ClientUser from the headers of a HTTP request.
 func getUser(req *http.Request) ClientUser {
-	var user ClientUser
-	userString := req.Header.Get("Sous-User")
-	if userString == "" {
-		user.Name = "Anonymous"
+	// Maybe we want to check this user isn't empty, eventually.
+	return ClientUser{
+		Name:  req.Header.Get("Sous-User-Name"),
+		Email: req.Header.Get("Sous-User-Email"),
 	}
-	parts := strings.SplitN(userString, "<", 2)
-	user.Name = strings.TrimSpace(parts[0])
-	if len(parts) == 2 {
-		user.Email = strings.TrimSpace(strings.TrimSuffix(parts[1], ">"))
-	}
-	return user
 }
