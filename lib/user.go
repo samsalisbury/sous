b60a1c6bb1d1a54007d96e5f57e1e710af92928a
Sam Salisbury <samsalisbury@gmail.com>
2017-02-03 10:54:49 +0000

    lib: Add env tags for SOUS_USER_NAME/EMAIL + doc.

--- lib/user.go
+++ lib/user.go
@@ -5,11 +5,16 @@ import "fmt"
 // User represents a user of the Sous client.
 type User struct {
 	// Name is the full name of this user.
-	Name,
+	Name string `env:"SOUS_USER_NAME"`
 	// Email is the email address of this user.
-	Email string
+	Email string `env:"SOUS_USER_EMAIL"`
 }
 
+// String returns the name and email in standard email address format, i.e.:
+//
+//     User Name <email@address.com>
+//
+// This is used directly by git commit --author, and is reasonable in logs etc.
 func (u User) String() string {
 	return fmt.Sprintf("%s <%s>", u.Name, u.Email)
 }
