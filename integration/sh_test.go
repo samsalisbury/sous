18045be098f35607d3a855b7ba0b9c2068c4b084
Judson Lester <jlester@opentable.com>
2017-02-14 10:57:50 -0800

    Need to specify GOROOT for Travis
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch cli_testing_tool
    # Your branch is up-to-date with 'nyarly/cli_testing_tool'.
    #
    # Changes to be committed:
    #	modified:   integration/sh_test.go
    #

--- integration/sh_test.go
+++ integration/sh_test.go
@@ -124,6 +124,7 @@ func buildShell(name string, t *testing.T) *shelltest.ShellTest {
 				"HOME":       cfg.Homedir,
 				"XDG_CONFIG": cfg.XDGConfig,
 				"GIT_SSH":    cfg.SSHWrapper,
+				"GOROOT":     cfg.GoPath[0],
 				"GOPATH":     strings.Join(cfg.GoPath, ":"),
 				"PATH":       strings.Join(cfg.ShellPath, ":"),
 			}))
