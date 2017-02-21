faa29e4b9e33a6f514fa0267ff12928429e99be1
Sam Salisbury <samsalisbury@gmail.com>
2017-02-21 14:22:54 +0000

    lib: DeployStates.Diff2

---
+++ lib/deploystates.go
@@ -0,0 +1,39 @@
+package sous
+
+import "fmt"
+
+// Diff2 calculates the differences between two DeployStates.
+func (ds DeployStates) Diff2(other DeployStates) (bool, []string) {
+	var diffs []string
+	diff := func(format string, a ...interface{}) { diffs = append(diffs, fmt.Sprintf(format, a...)) }
+
+	d := ds.Snapshot()
+	o := other.Snapshot()
+
+	if len(d) != len(o) {
+		diff("number of deploy states; this: %d, other: %d", len(d), len(o))
+	}
+
+	for did, thisDeployState := range d {
+		otherDeployState, ok := o[did]
+		if !ok {
+			diff("other missing deploy state %q", did)
+			continue
+		}
+		dsDifferent, dsDiffs := thisDeployState.Diff(otherDeployState)
+		if dsDifferent {
+			diff("differences in deploy state %q", did)
+			for _, dsDiff := range dsDiffs {
+				diff("in deploy state %q: %s", did, dsDiff)
+			}
+		}
+	}
+
+	for did := range o {
+		if _, ok := d[did]; !ok {
+			diff("other has extra deploy state %q", did)
+		}
+	}
+
+	return len(diffs) != 0, diffs
+}
