8595a03842f440797904027a3846f82efe12427c
Lars Lehtonen <lars.lehtonen@gmail.com>
2017-05-16 11:13:56 -0700

    Add /slowhealthy to webapp.py.

--- integration/webapp/webapp.py
+++ integration/webapp/webapp.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import os
+import time
 from bottle import Bottle, run, response
+
+BECOME_HEALTHY_SECONDS=60
+
+start_time = time.time()
 
 app = Bottle()
 
@@ -20,6 +25,18 @@ def sick():
     print("hit sick route.")
     response.status = 400
     return("sick")
+
+@app.route('/slowhealthy')
+def slowhealthy():
+    print("hit slow-healthy route.")
+    now = time.time()
+    elapsed = int(now - start_time)
+    if elapsed <= BECOME_HEALTHY_SECONDS:
+        response.status = 400
+        return "Still ill. Running time:{}s. Eventually healthy at:{}s".format(
+            elapsed, BECOME_HEALTHY_SECONDS)
+    return("Became healthy at {}s, {}s ago.".format(
+            BECOME_HEALTHY_SECONDS, (elapsed - BECOME_HEALTHY_SECONDS)))
 
 mesos_port = os.environ["PORT0"]
 mesos_host = os.environ["TASK_HOST"]
