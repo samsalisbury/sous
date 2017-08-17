759a35994f67566722e3911c8048c167b010b48d
Judson <nyarly@gmail.com>
2017-08-17 13:36:10 -0700

    Design notes

--- util/messages/messages.go
+++ util/messages/messages.go
@@ -1,6 +1,27 @@
 // The goal of this package is to integrate structured logging an metrics
 // reporting with error handling in an interface as close as possible to the
 // fluency of fmt.Errorf(...)
+
+// or of errors.Wrapf(err, "fmt", args...)
+
+// Concerns:
+//   a. structured logging using a defined scheme
+//   b. build-time checking of errors
+//   c. 3 purposes, which each message type can make use of 1-3 of:
+//     logging to ELK,
+//     metrics collection
+//     error reporting
+//   d. Contextualization - i.e. pull message fields from a context.Context
+//        or from a logging context likewise contextualized.
+
+// Nice to have:
+//   z. Output filtering disjoint from creation (i.e. *not* log.debug but rather debug stuff from the singularity API)
+//   y. Runtime output filtering, via e.g. HTTP requests.
+//   x. A live ringbuffer of all messages
+
+// b & d are in tension.
+// also, a with OTLs, because optional fields
+
 package messages
 
 type (
