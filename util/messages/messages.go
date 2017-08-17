959b0a39973bc565b55bc4c04680791cacf2361c
Judson <nyarly@gmail.com>
2017-08-17 10:50:24 -0700

    A note

--- util/messages/messages.go
+++ util/messages/messages.go
@@ -1,4 +1,7 @@
-package main
+// The goal of this package is to integrate structured logging an metrics
+// reporting with error handling in an interface as close as possible to the
+// fluency of fmt.Errorf(...)
+package messages
 
 type (
 	metricser interface{}
