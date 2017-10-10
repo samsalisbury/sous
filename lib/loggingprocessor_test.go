0c184290064953fa36e5d6aca20b487390f9ccaa
Judson <nyarly@gmail.com>
2017-10-09 23:11:31 -0700

    Skeleton test functions

--- lib/loggingprocessor_test.go
+++ lib/loggingprocessor_test.go
@@ -1,13 +1,13 @@
 package sous
 
 func TestDeliversDiffMessages(t *testing.T) {
-
 	// message for create, update, delete, samesies
+	t.Error("No test for diff messages delivery")
 	// message for errors
+	t.Error("Doesn't deliver messages for errors")
 }
 
 func TestDiffMessagesWork(t *testing.T) {
-
 	// the usual test helper
-
+	t.Error("No proof diff messages work")
 }
