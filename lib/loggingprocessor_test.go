aad6ad1b1d9871b7ccdece6cc4b20c05057b3bd4
Judson <nyarly@gmail.com>
2017-10-09 23:09:28 -0700

    Skeleton test functions

---
+++ lib/loggingprocessor_test.go
@@ -0,0 +1,13 @@
+package sous
+
+func TestDeliversDiffMessages(t *testing.T) {
+
+	// message for create, update, delete, samesies
+	// message for errors
+}
+
+func TestDiffMessagesWork(t *testing.T) {
+
+	// the usual test helper
+
+}
