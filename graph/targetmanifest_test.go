e863595ed819447065a3036788ef399506ee7f69
Sam Salisbury <samsalisbury@gmail.com>
2017-10-12 14:52:02 +0100

    graph: newRefinedResolveFilter tests cover offset
    - Should also add coverage for tag situations...

--- graph/targetmanifest_test.go
+++ graph/targetmanifest_test.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/opentable/sous/config"
@@ -253,4 +254,217 @@ func TestNewTargetManifest(t *testing.T) {
 		},
 	}
 	assert.Equal(t, expected, tm.Manifest)
+}
+
+func TestNewRefinedResolveFilter(t *testing.T) {
+	type In struct {
+		Filter     *sous.ResolveFilter
+		Discovered *SourceContextDiscovery
+	}
+
+	rrfTests := []struct {
+		Desc        string
+		In          In
+		ExpectPanic bool
+		ExpectErr   string
+		Expect      func(*RefinedResolveFilter) error
+	}{
+		{
+			Desc:        "nil inputs panics",
+			ExpectPanic: true,
+		},
+		{
+			Desc: "nil SourceContextDiscovery panics",
+			In: In{
+				Filter: &sous.ResolveFilter{},
+			},
+			ExpectPanic: true,
+		},
+		{
+			Desc: "no repo specified results in error",
+			In: In{
+				Discovered: &SourceContextDiscovery{},
+			},
+			ExpectErr: "no repo specified, please use -repo or run sous inside a git repo with a configured remote",
+		},
+		{
+			Desc: "no repo specified results in error",
+			In: In{
+				Discovered: &SourceContextDiscovery{},
+			},
+			ExpectErr: "no repo specified, please use -repo or run sous inside a git repo with a configured remote",
+		},
+		{
+			Desc: "detected repo results in success",
+			In: In{
+				Discovered: &SourceContextDiscovery{
+					SourceContext: &sous.SourceContext{
+						PrimaryRemoteURL: "github.com/a/b",
+					},
+				},
+			},
+			Expect: func(rrf *RefinedResolveFilter) error {
+				expectedRepo := "github.com/a/b"
+				if rrf.Repo.All() {
+					return fmt.Errorf("got ALL; want %q", expectedRepo)
+				}
+				actual := *rrf.Repo.Match
+				if actual != expectedRepo {
+					return fmt.Errorf("got %q; want %q", actual, expectedRepo)
+				}
+				return nil
+			},
+		},
+		{
+			Desc: "flag repo overrides detected",
+			In: In{
+				Discovered: &SourceContextDiscovery{
+					SourceContext: &sous.SourceContext{
+						PrimaryRemoteURL: "github.com/from/context",
+					},
+				},
+				Filter: &sous.ResolveFilter{
+					Repo: sous.NewResolveFieldMatcher("github.com/from/flags"),
+				},
+			},
+			Expect: func(rrf *RefinedResolveFilter) error {
+				expectedRepo := "github.com/from/flags"
+				if rrf.Repo.All() {
+					return fmt.Errorf("got ALL; want %q", expectedRepo)
+				}
+				actual := *rrf.Repo.Match
+				if actual != expectedRepo {
+					return fmt.Errorf("got %q; want %q", actual, expectedRepo)
+				}
+				return nil
+			},
+		},
+		{
+			Desc: "flag offset overrides detected offset",
+			In: In{
+				Discovered: &SourceContextDiscovery{
+					SourceContext: &sous.SourceContext{
+						PrimaryRemoteURL: "github.com/from/context",
+						OffsetDir:        "from/context",
+					},
+				},
+				Filter: &sous.ResolveFilter{
+					Offset: sous.NewResolveFieldMatcher("from/flags"),
+				},
+			},
+			Expect: func(rrf *RefinedResolveFilter) error {
+				expectedOffset := "from/flags"
+				if rrf.Offset.All() {
+					return fmt.Errorf("got ALL; want %q", expectedOffset)
+				}
+				actual := *rrf.Offset.Match
+				if actual != expectedOffset {
+					return fmt.Errorf("got %q; want %q", actual, expectedOffset)
+				}
+				return nil
+			},
+		},
+		{
+			Desc: "flag repo sets detected offset to empty",
+			In: In{
+				Discovered: &SourceContextDiscovery{
+					SourceContext: &sous.SourceContext{
+						PrimaryRemoteURL: "github.com/from/context",
+						OffsetDir:        "from/context",
+					},
+				},
+				Filter: &sous.ResolveFilter{
+					Repo: sous.NewResolveFieldMatcher("github.com/from/flags"),
+				},
+			},
+			Expect: func(rrf *RefinedResolveFilter) error {
+				expectedOffset := ""
+				if rrf.Offset.All() {
+					return fmt.Errorf("got ALL; want %q", expectedOffset)
+				}
+				actual := *rrf.Offset.Match
+				if actual != expectedOffset {
+					return fmt.Errorf("got %q; want %q", actual, expectedOffset)
+				}
+				return nil
+			},
+		},
+		{
+			Desc: "flag repo and offset override sets detected",
+			In: In{
+				Discovered: &SourceContextDiscovery{
+					SourceContext: &sous.SourceContext{
+						PrimaryRemoteURL: "github.com/from/context",
+						OffsetDir:        "from/context",
+					},
+				},
+				Filter: &sous.ResolveFilter{
+					Repo:   sous.NewResolveFieldMatcher("github.com/from/flags"),
+					Offset: sous.NewResolveFieldMatcher("from/flags"),
+				},
+			},
+			Expect: func(rrf *RefinedResolveFilter) error {
+				expectedRepo := "github.com/from/flags"
+				if rrf.Repo.All() {
+					return fmt.Errorf("got ALL; want %q", expectedRepo)
+				}
+				actualRepo := *rrf.Repo.Match
+				if actualRepo != expectedRepo {
+					return fmt.Errorf("got %q; want %q", actualRepo, expectedRepo)
+				}
+				expectedOffset := "from/flags"
+				if rrf.Offset.All() {
+					return fmt.Errorf("got ALL; want %q", expectedOffset)
+				}
+				actual := *rrf.Offset.Match
+				if actual != expectedOffset {
+					return fmt.Errorf("got %q; want %q", actual, expectedOffset)
+				}
+				return nil
+			},
+		},
+	}
+
+	for _, test := range rrfTests {
+		t.Run("", func(t *testing.T) {
+			t.Run(test.Desc, func(t *testing.T) {
+				t.Parallel()
+				if !test.ExpectPanic && test.ExpectErr == "" && test.Expect == nil {
+					t.Fatalf("test case must have ExpectPanic, ExpectErr or Expect")
+				}
+				var recovered interface{}
+				var actual *RefinedResolveFilter
+				var err error
+				func() {
+					defer func() { recovered = recover() }()
+					actual, err = newRefinedResolveFilter(test.In.Filter, test.In.Discovered)
+				}()
+				if recovered != nil && !test.ExpectPanic {
+					t.Fatalf("got panic (%T): %+v", recovered, recovered)
+				}
+				if test.ExpectPanic && recovered == nil {
+					t.Fatal("did not panic; want panic")
+				}
+				if test.ExpectErr != "" && err == nil {
+					t.Fatalf("got nil error; want %q", test.ExpectErr)
+				}
+				if test.ExpectErr == "" && err != nil {
+					t.Fatalf("got error %q", err.Error())
+				}
+				if test.ExpectErr != "" && err != nil {
+					actualErr := err.Error()
+					if test.ExpectErr != actualErr {
+						t.Fatalf("got error %q; want %q", actualErr, test.ExpectErr)
+					}
+				}
+				if test.Expect == nil {
+					return
+				}
+				if err := test.Expect(actual); err != nil {
+					t.Fatal(err)
+				}
+			})
+		})
+	}
+
 }
