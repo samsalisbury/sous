4e23b5f70195e67c009e60cc305646daba2f0fab
Judson <nyarly@gmail.com>
2017-11-09 11:00:52 -0800

    Added tests for kafkasink

---
+++ util/logging/kafkasink_test.go
@@ -0,0 +1,22 @@
+package logging
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestKafkaSinkShouldSend(t *testing.T) {
+	testcase := func(msg, sink Level, should bool) {
+		t.Run(fmt.Sprintf("Sink %s Msg %s -> %t", msg, sink, should), func(t *testing.T) {
+			s := kafkaSink{level: sink}
+			actual := s.shouldSend(msg)
+			if actual != should {
+				t.Errorf("s.shouldSend(%s) -> %t, not %t", msg, actual, should)
+			}
+		})
+	}
+
+	testcase(InformationLevel, WarningLevel, false)
+	testcase(WarningLevel, WarningLevel, true)
+	testcase(CriticalLevel, WarningLevel, true)
+}
