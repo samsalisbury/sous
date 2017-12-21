ff9c3718f2bba28ddfb17c4a4e09c7aad8430eb4
Sam Salisbury <samsalisbury@gmail.com>
2017-12-21 14:21:58 +0000

    lib: Rectification queue.

--- lib/r11n_queue.go
+++ lib/r11n_queue.go
@@ -4,15 +4,46 @@ import "github.com/pborman/uuid"
 
 // R11nQueue is a queue of rectifications.
 type R11nQueue struct {
+	cap   int
 	queue chan *QueuedR11n
 	refs  map[R11nID]*QueuedR11n
+}
+
+// R11nQueueCapDefault is the default capacity for a new R11nQueue.
+const R11nQueueCapDefault = 10
+
+// NewR11nQueue creates a freshly initialised R11nQueue.
+func NewR11nQueue(opts ...R11nQueueOpt) *R11nQueue {
+	rq := &R11nQueue{
+		cap: R11nQueueCapDefault,
+	}
+	for _, opt := range opts {
+		opt(rq)
+	}
+	return rq.init()
+}
+
+// R11nQueueOpt is an option for configuring an R11nQueue.
+type R11nQueueOpt func(*R11nQueue)
+
+// R11nQueueCap sets the max capacity of an R11nQueue to the supplied cap.
+func R11nQueueCap(cap int) R11nQueueOpt {
+	return func(rq *R11nQueue) {
+		rq.cap = cap
+	}
+}
+
+func (rq *R11nQueue) init() *R11nQueue {
+	rq.queue = make(chan *QueuedR11n, rq.cap)
+	rq.refs = map[R11nID]*QueuedR11n{}
+	return rq
 }
 
 // QueuedR11n is a queue item wrapping a Rectification with an ID and position.
 type QueuedR11n struct {
 	ID            R11nID
 	Pos           int
-	Rectification Rectification
+	Rectification *Rectification
 }
 
 // R11nID is a QueuedR11n identifier.
@@ -23,34 +54,32 @@ func NewR11nID() R11nID {
 	return R11nID(uuid.New())
 }
 
-// NewR11nQueue creates a freshly initialised R11nQueue.
-func NewR11nQueue() *R11nQueue {
-	return &R11nQueue{
-		queue: make(chan *QueuedR11n, 10),
-		refs:  map[R11nID]*QueuedR11n{},
-	}
-}
-
 // Push adds r to the queue, wrapped in a *QueuedR11n. It returns the wrapper.
-func (rq *R11nQueue) Push(r *Rectification) *QueuedR11n {
+// If the push was successful, it returns the wrapper and true, otherwise it
+// returns nil and false.
+func (rq *R11nQueue) Push(r *Rectification) (*QueuedR11n, bool) {
+	if len(rq.queue) == rq.cap {
+		return nil, false
+	}
 	id := NewR11nID()
 	qr := &QueuedR11n{
-		ID:  id,
-		Pos: len(rq.queue),
+		ID:            id,
+		Pos:           len(rq.queue),
+		Rectification: r,
 	}
 	rq.refs[id] = qr
 	rq.queue <- qr
-	return qr
+	return qr, true
 }
 
 // PushIfEmpty adds an item to the queue if it is empty, and returns the wrapper
-// added and true if successful. If the queue is not empty, it returns
-// nil, false.
+// added and true if successful. If the queue is not empty, or is full, it
+// returns nil, false.
 func (rq *R11nQueue) PushIfEmpty(r *Rectification) (*QueuedR11n, bool) {
 	if len(rq.queue) != 0 {
 		return nil, false
 	}
-	return rq.Push(r), true
+	return rq.Push(r)
 }
 
 // Len returns the current number of items in the queue.
@@ -65,9 +94,9 @@ func (rq *R11nQueue) Pop() (*QueuedR11n, bool) {
 		return nil, false
 	}
 	qr := <-rq.queue
-	delete(rq.refs, qr.ID)
 	for _, r := range rq.refs {
 		r.Pos--
 	}
+	delete(rq.refs, qr.ID)
 	return qr, true
 }
--- lib/r11n_queue_test.go
+++ lib/r11n_queue_test.go
@@ -6,56 +6,166 @@ import (
 )
 
 // Test synchronous behaviour of the queue.
-func TestR11nQueue_synchronous(t *testing.T) {
+func TestR11nQueue_Push_Pop_sync(t *testing.T) {
+
+	// makeTestR11nWithRepo creates a test rectification with
+	// Pair.Post.Deployment.SourceID.Location.Repo == repo.
+	// This is enough to check identity of the r11n using
+	// checkPoppedR11nHasRepo.
+	makeTestR11nWithRepo := func(repo string) *Rectification {
+		return &Rectification{
+			Pair: DeployablePair{
+				Post: &Deployable{
+					Status: 0,
+					Deployment: &Deployment{
+						SourceID: SourceID{
+							Location: SourceLocation{
+								Repo: repo,
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+	// checkPoppedR11nHasRepo checks identity of r11ns created with
+	// makeTestR11nWithRepo.
+	checkPoppedR11nHasRepo := func(repo string) func(*QueuedR11n) error {
+		return func(qr *QueuedR11n) error {
+			got := qr.Rectification.Pair.Post.Deployment.SourceID.Location.Repo
+			if got != repo {
+				return fmt.Errorf("got %q; want %q", got, repo)
+			}
+			return nil
+		}
+	}
 
 	testCases := []struct {
 		// Desc is a short description of the test.
 		Desc string
-		// WantLen is the expected result of Len after all the items in Push
-		// have been pushed.
+		// Init is the queue to start with for this test.
+		Init *R11nQueue
+		// WantLen is the length we expect the queue to be after all items in
+		// Push have been pushed onto it. If zero, this defaults to the number
+		// of items in Push.
 		WantLen int
 		// Push is a slice of R11ns that are pushed onto the queue one at a
 		// time.
 		Push []*Rectification
-		// AssertPushed is called on each item returned from Push calls.
-		WantPushed,
-		// AssertPeek funcs are run one at a time and passed the result of
-		// Peeking at the next item, which is then popped.
-		WantPeeked []func(*QueuedR11n) error
+		// WantPushed funcs are run one at a time after each Push call, and
+		// passed the returned values from Push. If they return an error, the
+		// test has the error added.
+		WantPushed []func(*QueuedR11n, bool) error
+		// WantPoppedOK funcs are run one at a time after calling Pop.
+		// If Pop returns nil or false, the corresponding WantPoppedOK is not
+		// run, and a test error is generated instead.
+		WantPoppedOK []func(*QueuedR11n) error
 	}{
 		{
-			Desc:    "new queue",
-			WantLen: 0,
+			Desc: "default empty queue; no Push",
+			Init: NewR11nQueue(),
 		},
 		{
-			Desc: "one zero item",
+			Desc: "default queue; Push one item",
+			Init: NewR11nQueue(),
 			Push: []*Rectification{
-				&Rectification{},
+				makeTestR11nWithRepo("one"),
 			},
-			WantLen: 1,
+			WantPoppedOK: []func(*QueuedR11n) error{
+				checkPoppedR11nHasRepo("one"),
+			},
 		},
 		{
-			Desc: "two zero items",
+			Desc: "default queue; Push two items",
+			Init: NewR11nQueue(),
 			Push: []*Rectification{
-				&Rectification{},
-				&Rectification{},
+				makeTestR11nWithRepo("one"),
+				makeTestR11nWithRepo("two"),
 			},
-			WantLen: 2,
+			WantPoppedOK: []func(*QueuedR11n) error{
+				checkPoppedR11nHasRepo("one"),
+				checkPoppedR11nHasRepo("two"),
+			},
+		},
+		{
+			Desc: "queue cap one; Push two items",
+			Init: NewR11nQueue(R11nQueueCap(1)),
+			Push: []*Rectification{
+				makeTestR11nWithRepo("one"),
+				makeTestR11nWithRepo("two"),
+			},
+			WantLen: 1, // Can't go above capacity.
+			WantPushed: []func(*QueuedR11n, bool) error{
+				nil, // No custom check for the first one.
+				func(qr *QueuedR11n, ok bool) error {
+					if qr != nil {
+						return fmt.Errorf("got QueuedR11n %s; want nil", qr.ID)
+					}
+					if ok {
+						return fmt.Errorf("got ok true; want false")
+					}
+					return nil
+				},
+			},
+			WantPoppedOK: []func(*QueuedR11n) error{
+				checkPoppedR11nHasRepo("one"),
+			},
+		},
+		{
+			Desc: "queue cap two; Push three items",
+			Init: NewR11nQueue(R11nQueueCap(2)),
+			Push: []*Rectification{
+				makeTestR11nWithRepo("one"),
+				makeTestR11nWithRepo("two"),
+				makeTestR11nWithRepo("three"),
+			},
+			WantLen: 2, // Can't go above capacity.
+			WantPushed: []func(*QueuedR11n, bool) error{
+				nil,
+				nil, // No custom checks for the first or second ones.
+				func(qr *QueuedR11n, ok bool) error {
+					if qr != nil {
+						return fmt.Errorf("got QueuedR11n %s; want nil", qr.ID)
+					}
+					if ok {
+						return fmt.Errorf("got ok true; want false")
+					}
+					return nil
+				},
+			},
+			WantPoppedOK: []func(*QueuedR11n) error{
+				checkPoppedR11nHasRepo("one"),
+				checkPoppedR11nHasRepo("two"),
+			},
 		},
 	}
 
 	for _, tc := range testCases {
+		tc := tc // Needed since we're using t.Parallel below.
 		t.Run(tc.Desc, func(t *testing.T) {
 			// Top-level can be run in parallel, further sub-tests must not be
 			// as they rely on pushing and popping in a certain order.
 			t.Parallel()
-			rq := NewR11nQueue()
+			rq := tc.Init
 			pushedIDsOrder := make(map[R11nID]int, 10)
 			// Push each item to be pushed.
 			for i, r11n := range tc.Push {
 				desc := fmt.Sprintf("pushed %d", i)
 				t.Run(desc, func(t *testing.T) {
-					pushed := rq.Push(r11n)
+
+					pushed, ok := rq.Push(r11n)
+
+					// If there is a non-nil check for this Push call, run it.
+					if len(tc.WantPushed) > i && tc.WantPushed[i] != nil {
+						if err := tc.WantPushed[i](pushed, ok); err != nil {
+							t.Error(err)
+						}
+					}
+
+					// If push wasn't ok, stop running checks.
+					if !ok || pushed == nil {
+						return
+					}
 
 					// Check each ID is unique.
 					if _, ok := pushedIDsOrder[pushed.ID]; ok {
@@ -68,53 +178,66 @@ func TestR11nQueue_synchronous(t *testing.T) {
 						t.Errorf("got pos %d; want %d", pushed.Pos, i)
 					}
 
-					// If there is an assertion for this pushed item, run it.
-					if len(tc.WantPushed) <= i {
-						return
-					}
-					if err := tc.WantPushed[i](pushed); err != nil {
-						t.Error(err)
-					}
 				})
 			}
 
 			// Check length after pushing all items.
 			gotLen, wantLen := rq.Len(), tc.WantLen
+			if wantLen == 0 {
+				// Default to length of Push if tc.WantLen is zero.
+				wantLen = len(tc.Push)
+			}
 			if gotLen != wantLen {
 				t.Errorf("got len %d; want %d", gotLen, wantLen)
 			}
 
-			// Iterate over each peeked item.
+			// Iterate over each popped item.
 			i := 0
-			for peeked, ok := rq.Pop(); ok; peeked, ok = rq.Pop() {
+			for popped, ok := rq.Pop(); ok; popped, ok = rq.Pop() {
 				desc := fmt.Sprintf("popped %d", i)
 				t.Run(desc, func(t *testing.T) {
 
-					// Check peeked always has position 0.
-					if peeked.Pos != 0 {
-						t.Errorf("got position %d; want %d", peeked.Pos, 0)
+					// Check popped always has position -1.
+					if popped.Pos != -1 {
+						t.Errorf("got position %d; want %d", popped.Pos, -1)
 					}
 
-					// Check peeked order is the same as pushed order.
-					if pushedOrder, ok := pushedIDsOrder[peeked.ID]; ok {
+					// Check popped order is the same as pushed order.
+					if pushedOrder, ok := pushedIDsOrder[popped.ID]; ok {
 						if pushedOrder != i {
 							t.Fatalf("popped %q at %d; want it at %d",
-								peeked.ID, i, pushedOrder)
+								popped.ID, i, pushedOrder)
 						}
 					} else {
-						t.Fatalf("popped un-pushed ID %q", peeked.ID)
+						t.Fatalf("popped un-pushed ID %q", popped.ID)
 					}
 
-					// If there is an assertion for this peeked item, run it.
-					if len(tc.WantPeeked) <= i {
+					// If there is a check for this popped item, run it.
+					if len(tc.WantPoppedOK) <= i {
 						return
 					}
-					if err := tc.WantPeeked[i](peeked); err != nil {
+					// Popped assertions always assume we get something popped.
+					if !ok {
+						t.Fatalf("got !ok; want ok for WantPoppedOK")
+					}
+					if popped == nil {
+						t.Fatalf("got nil QueuedR11n; want not nil")
+					}
+					if err := tc.WantPoppedOK[i](popped); err != nil {
 						t.Error(err)
 					}
 				})
 
 				i++
+			}
+
+			// Now the queue is empty, Pop should return nil, false.
+			popped, ok := rq.Pop()
+			if popped != nil {
+				t.Errorf("got QueuedR11n %s; want nil", popped.ID)
+			}
+			if ok {
+				t.Errorf("got ok true; want false")
 			}
 		})
 	}
