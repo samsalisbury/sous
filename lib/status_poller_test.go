3c2c4c253ee9ffa662569c9c1d935c94c88c75a2
jc1738 <jcastillo@gmail.com>
2018-01-25 16:57:26 -0800

    change on error marshal, need to make sure if get's called with empty internal error, doesn't fail

--- lib/status_poller_test.go
+++ lib/status_poller_test.go
@@ -43,37 +43,6 @@ func TestResolveState_String(t *testing.T) {
 	}
 }
 
-// func TestReportCHResponseFields(t *testing.T) {
-// 	logger, control := logging.NewLogSinkSpy()
-// 	msg := newHTTPLogEntry(false, "example-api", "GET", "http://example.com/api?a=a", 200, 0, 123, time.Millisecond*30)
-// 	logging.Deliver(msg, logger)
-
-// 	assert.Len(t, control.Metrics.CallsTo("UpdateTimer"), 3)
-// 	assert.Len(t, control.Metrics.CallsTo("UpdateSample"), 6)
-// 	assert.Len(t, control.Metrics.CallsTo("IncCounter"), 3)
-// 	logCalls := control.CallsTo("LogMessage")
-// 	require.Len(t, logCalls, 1)
-// 	assert.Equal(t, logCalls[0].PassedArgs().Get(0), logging.InformationLevel)
-// 	message := logCalls[0].PassedArgs().Get(1).(logging.LogMessage)
-
-// 	fixedFields := map[string]interface{}{
-// 		"@loglov3-otl":    "sous-http-v1",
-// 		"incoming":        false,
-// 		"resource-family": "example-api",
-// 		"method":          "GET",
-// 		"url":             "http://example.com/api?a=a",
-// 		"url-hostname":    "example.com",
-// 		"url-pathname":    "/api",
-// 		"url-querystring": "a=a",
-// 		"duration":        int64(30000),
-// 		"body-size":       int64(0),
-// 		"response-size":   int64(123),
-// 		"status":          200,
-// 	}
-
-// 	logging.AssertMessageFields(t, message, logging.StandardVariableFields, fixedFields)
-// }
-
 func makeTestDep() *Deployable {
 	return &Deployable{
 		Status: DeployStatusActive,
