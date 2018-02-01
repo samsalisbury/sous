b13f370e18102652a57906661641986c62f72ac6
Sam Salisbury <samsalisbury@gmail.com>
2018-02-01 15:54:23 +0000

    lib: TestDeploymentID_String

--- lib/deploymentid_test.go
+++ lib/deploymentid_test.go
@@ -22,3 +22,72 @@ func TestDeploymentID_Digest(t *testing.T) {
 	}
 	t.Logf("success: %q mapped to %q", got, want)
 }
+
+func TestDeploymentID_String(t *testing.T) {
+	testCases := []struct {
+		desc string
+		in   DeploymentID
+		want string
+	}{
+		{
+			desc: "zero DeploymentID",
+			in:   DeploymentID{},
+			want: ":",
+		},
+		{
+			desc: "cluster",
+			in: DeploymentID{
+				Cluster: "cluster1",
+			},
+			want: "cluster1:",
+		},
+		{
+			desc: "cluster-repo",
+			in: DeploymentID{
+				ManifestID: ManifestID{
+					Source: SourceLocation{
+						Repo: "repo1",
+					},
+				},
+				Cluster: "cluster1",
+			},
+			want: "cluster1:repo1",
+		},
+		{
+			desc: "cluster-repo-dir",
+			in: DeploymentID{
+				ManifestID: ManifestID{
+					Source: SourceLocation{
+						Repo: "repo1",
+						Dir:  "dir1",
+					},
+				},
+				Cluster: "cluster1",
+			},
+			want: "cluster1:repo1,dir1",
+		},
+		{
+			desc: "cluster-repo-dir-flavor",
+			in: DeploymentID{
+				ManifestID: ManifestID{
+					Source: SourceLocation{
+						Repo: "repo1",
+						Dir:  "dir1",
+					},
+					Flavor: "flavor1",
+				},
+				Cluster: "cluster1",
+			},
+			want: "cluster1:repo1,dir1~flavor1",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := tc.in.String()
+			if got != tc.want {
+				t.Errorf("got %q; want %q", got, tc.want)
+			}
+		})
+	}
+}
