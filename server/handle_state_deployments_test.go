9e13481a5f8d619c7d53da6b63ccab2b1d265b5f
Judson <nyarly@gmail.com>
2018-02-05 15:27:06 -0800

    Tests for PUT

--- server/handle_state_deployments_test.go
+++ server/handle_state_deployments_test.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"testing"
 
 	"github.com/nyarly/spies"
@@ -38,10 +41,47 @@ func TestGetStateDeployments(t *testing.T) {
 	if len(gdm.Deployments) != 3 {
 		t.Errorf("Expected 3 Deployments, got %d", len(gdm.Deployments))
 	}
-
 }
 
 func TestPutStateDeployments(t *testing.T) {
+	cm, ctrl := sous.NewClusterManagerSpy()
+	gdm := GDMWrapper{
+		Deployments: []*sous.Deployment{
+			sous.DeploymentFixture("sequenced-repo"),
+			sous.DeploymentFixture("sequenced-repo"),
+			sous.DeploymentFixture("sequenced-repo"),
+		},
+	}
+
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+	enc.Encode(gdm)
+
+	req, err := http.NewRequest("PUT", "", buf)
+	if err != nil {
+		t.Fatal("error building request", err)
+	}
+
+	ex := &PUTStateDeployments{
+		cluster:     cm,
+		clusterName: "test-cluster",
+		req:         req,
+	}
+
+	data, status := ex.Exchange()
+
+	if status != 202 {
+		t.Fatalf("Expected 200 status, got %d", status)
+	}
+
+	if data != nil {
+		t.Fatalf("Expect nil data was %#v", data)
+	}
+
+	if len(ctrl.CallsTo("WriteCluster")) == 0 {
+		t.Errorf("No calls to WriteCluster")
+	}
+
 }
 
 func TestRoundtripStateDeployments(t *testing.T) {
