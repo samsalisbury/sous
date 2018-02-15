0765f74e84708a813e109d52d04575fcac555a24
Jim Castillo <jcastillo@gmail.com>
2018-02-15 09:49:01 -0800

    fix tests with rename of field name

--- util/logging/messages/generalmsg_test.go
+++ util/logging/messages/generalmsg_test.go
@@ -20,7 +20,7 @@ func TestReportLogFieldsMessage_Complete(t *testing.T) {
 		map[string]interface{}{
 			"fields":       "Basic,Kafka,Graphite,Config,Level,DisableConsole,Enabled,DefaultLevel,Topic,Brokers,BrokerList,Server",
 			"types":        "Config,string,bool",
-			"jsonStruct":   "{\"message\":{\"array\":[\"(logging.Config) {\\n Basic: (struct { Level string \\\"env:\\\\\\\"SOUS_LOGGING_LEVEL\\\\\\\"\\\"; DisableConsole bool }) {\\n  Level: (string) \\\"\\\",\\n  DisableConsole: (bool) false\\n },\\n Kafka: (struct { Enabled bool; DefaultLevel string \\\"env:\\\\\\\"SOUS_KAFKA_LOG_LEVEL\\\\\\\"\\\"; Topic string \\\"env:\\\\\\\"SOUS_KAFKA_TOPIC\\\\\\\"\\\"; Brokers []string; BrokerList string \\\"env:\\\\\\\"SOUS_KAFKA_BROKERS\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  DefaultLevel: (string) \\\"\\\",\\n  Topic: (string) (len=10) \\\"test-topic\\\",\\n  Brokers: ([]string) \\u003cnil\\u003e,\\n  BrokerList: (string) (len=23) \\\"broker1,broker2,broker3\\\"\\n },\\n Graphite: (struct { Enabled bool; Server string \\\"env:\\\\\\\"SOUS_GRAPHITE_SERVER\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  Server: (string) \\\"\\\"\\n }\\n}\\n\"]}}",
+			"json-value":   "{\"message\":{\"array\":[\"(logging.Config) {\\n Basic: (struct { Level string \\\"env:\\\\\\\"SOUS_LOGGING_LEVEL\\\\\\\"\\\"; DisableConsole bool }) {\\n  Level: (string) \\\"\\\",\\n  DisableConsole: (bool) false\\n },\\n Kafka: (struct { Enabled bool; DefaultLevel string \\\"env:\\\\\\\"SOUS_KAFKA_LOG_LEVEL\\\\\\\"\\\"; Topic string \\\"env:\\\\\\\"SOUS_KAFKA_TOPIC\\\\\\\"\\\"; Brokers []string; BrokerList string \\\"env:\\\\\\\"SOUS_KAFKA_BROKERS\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  DefaultLevel: (string) \\\"\\\",\\n  Topic: (string) (len=10) \\\"test-topic\\\",\\n  Brokers: ([]string) \\u003cnil\\u003e,\\n  BrokerList: (string) (len=23) \\\"broker1,broker2,broker3\\\"\\n },\\n Graphite: (struct { Enabled bool; Server string \\\"env:\\\\\\\"SOUS_GRAPHITE_SERVER\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  Server: (string) \\\"\\\"\\n }\\n}\\n\"]}}",
 			"@loglov3-otl": "sous-generic-v1",
 		})
 }
@@ -34,7 +34,7 @@ func TestReportLogFieldsMessage_NoInterface(t *testing.T) {
 		map[string]interface{}{
 			"fields":       "",
 			"types":        "",
-			"jsonStruct":   "{\"message\":{\"array\":[]}}",
+			"json-value":   "{\"message\":{\"array\":[]}}",
 			"@loglov3-otl": "sous-generic-v1",
 		})
 }
@@ -47,7 +47,7 @@ func TestReportLogFieldsMessage_String(t *testing.T) {
 		map[string]interface{}{
 			"fields":       "",
 			"types":        "string",
-			"jsonStruct":   "{\"message\":{\"array\":[\"{\\\"string\\\":{\\\"string\\\":\\\"simple string\\\"}}\"]}}",
+			"json-value":   "{\"message\":{\"array\":[\"{\\\"string\\\":{\\\"string\\\":\\\"simple string\\\"}}\"]}}",
 			"@loglov3-otl": "sous-generic-v1",
 		})
 }
@@ -65,13 +65,13 @@ func TestReportLogFieldsMessage_StructAndString(t *testing.T) {
 		map[string]interface{}{
 			"fields":       "Basic,Kafka,Graphite,Config,Level,DisableConsole,Enabled,DefaultLevel,Topic,Brokers,BrokerList,Server",
 			"types":        "Config,string,bool",
-			"jsonStruct":   "{\"message\":{\"array\":[\"(logging.Config) {\\n Basic: (struct { Level string \\\"env:\\\\\\\"SOUS_LOGGING_LEVEL\\\\\\\"\\\"; DisableConsole bool }) {\\n  Level: (string) \\\"\\\",\\n  DisableConsole: (bool) false\\n },\\n Kafka: (struct { Enabled bool; DefaultLevel string \\\"env:\\\\\\\"SOUS_KAFKA_LOG_LEVEL\\\\\\\"\\\"; Topic string \\\"env:\\\\\\\"SOUS_KAFKA_TOPIC\\\\\\\"\\\"; Brokers []string; BrokerList string \\\"env:\\\\\\\"SOUS_KAFKA_BROKERS\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  DefaultLevel: (string) \\\"\\\",\\n  Topic: (string) (len=10) \\\"test-topic\\\",\\n  Brokers: ([]string) \\u003cnil\\u003e,\\n  BrokerList: (string) (len=23) \\\"broker1,broker2,broker3\\\"\\n },\\n Graphite: (struct { Enabled bool; Server string \\\"env:\\\\\\\"SOUS_GRAPHITE_SERVER\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  Server: (string) \\\"\\\"\\n }\\n}\\n\",\"{\\\"string\\\":{\\\"string\\\":\\\"simple string\\\"}}\"]}}",
+			"json-value":   "{\"message\":{\"array\":[\"(logging.Config) {\\n Basic: (struct { Level string \\\"env:\\\\\\\"SOUS_LOGGING_LEVEL\\\\\\\"\\\"; DisableConsole bool }) {\\n  Level: (string) \\\"\\\",\\n  DisableConsole: (bool) false\\n },\\n Kafka: (struct { Enabled bool; DefaultLevel string \\\"env:\\\\\\\"SOUS_KAFKA_LOG_LEVEL\\\\\\\"\\\"; Topic string \\\"env:\\\\\\\"SOUS_KAFKA_TOPIC\\\\\\\"\\\"; Brokers []string; BrokerList string \\\"env:\\\\\\\"SOUS_KAFKA_BROKERS\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  DefaultLevel: (string) \\\"\\\",\\n  Topic: (string) (len=10) \\\"test-topic\\\",\\n  Brokers: ([]string) \\u003cnil\\u003e,\\n  BrokerList: (string) (len=23) \\\"broker1,broker2,broker3\\\"\\n },\\n Graphite: (struct { Enabled bool; Server string \\\"env:\\\\\\\"SOUS_GRAPHITE_SERVER\\\\\\\"\\\" }) {\\n  Enabled: (bool) false,\\n  Server: (string) \\\"\\\"\\n }\\n}\\n\",\"{\\\"string\\\":{\\\"string\\\":\\\"simple string\\\"}}\"]}}",
 			"@loglov3-otl": "sous-generic-v1",
 		})
 }
 
 //normally wouldn't use this logger with http response, but this was just done to test logging of a very complex structure
-//and ensure it didn't fail ot going to put jsonStruct as expected, since it contains pointers that can change on run
+//and ensure it didn't fail ot going to put json-value as expected, since it contains pointers that can change on run
 //execution
 func TestReportLogFieldsMessage_TwoStructs(t *testing.T) {
 	logging.AssertReportFields(t,
@@ -82,7 +82,7 @@ func TestReportLogFieldsMessage_TwoStructs(t *testing.T) {
 			res := buildHTTPResponse(t, "GET", "http://example.com/api?a=a", 200, 0, 123)
 			ReportLogFieldsMessage("This is test message", logging.DebugLevel, ls, cfg, res)
 		},
-		append(logging.StandardVariableFields, "jsonStruct"),
+		append(logging.StandardVariableFields, "json-value"),
 		map[string]interface{}{
 			"fields":       "Basic,Kafka,Graphite,Config,Level,DisableConsole,Enabled,DefaultLevel,Topic,Brokers,BrokerList,Server,Status,StatusCode,Proto,ProtoMajor,ProtoMinor,Header,Body,ContentLength,TransferEncoding,Close,Uncompressed,Trailer,Request,TLS,Response", //nolint
 			"types":        "Config,string,bool,*Response,int,Header,int64,*Request,*ConnectionState",
@@ -103,7 +103,7 @@ func TestReportLogFieldsMessage_CyclicalReference(t *testing.T) {
 			myVar.Child = &myVar
 			ReportLogFieldsMessageToConsole("This is test message", logging.DebugLevel, ls, myVar)
 		},
-		append(logging.StandardVariableFields, "jsonStruct"),
+		append(logging.StandardVariableFields, "json-value"),
 		map[string]interface{}{
 			"fields":       "Child,LogData,Parent",
 			"types":        "Parent,*Parent,string",
@@ -122,7 +122,7 @@ func TestReportLogFieldsMessage_error(t *testing.T) {
 		map[string]interface{}{
 			"fields":       "",
 			"types":        "error",
-			"jsonStruct":   "{\"message\":{\"array\":[\"{\\\"error\\\":{\\\"error\\\":\\\"error msg\\\"}}\"]}}",
+			"json-value":   "{\"message\":{\"array\":[\"{\\\"error\\\":{\\\"error\\\":\\\"error msg\\\"}}\"]}}",
 			"@loglov3-otl": "sous-generic-v1",
 		})
 }
