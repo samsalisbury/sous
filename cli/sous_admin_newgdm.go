41c25f705bd42fd82ef5eaf7b41b7ec55f62410d
Sam Salisbury <samsalisbury@gmail.com>
2018-02-16 22:09:43 +0000

    cli: sous new-test-gdm more complete

--- cli/sous_admin_newgdm.go
+++ cli/sous_admin_newgdm.go
@@ -2,12 +2,15 @@ package cli
 
 import (
 	"flag"
+	"fmt"
+	"strings"
 
 	"github.com/opentable/sous/config"
 	"github.com/opentable/sous/ext/storage"
 	"github.com/opentable/sous/graph"
 	"github.com/opentable/sous/lib"
 	"github.com/opentable/sous/util/cmdr"
+	"github.com/opentable/sous/util/firsterr"
 )
 
 // SousNewGDM creates a new GDM at the configured StateLocation.
@@ -17,8 +20,10 @@ type SousNewGDM struct {
 	Config      graph.LocalSousConfig
 	User        sous.User
 	flags       struct {
-		CheckoutDir string
-		RemoteDir   string
+		CheckoutDir,
+		Clusters,
+		SingularityURL,
+		DockerRegURL string
 	}
 }
 
@@ -27,7 +32,7 @@ func init() { TopLevelCommands["new-test-gdm"] = &SousNewGDM{} }
 
 const sousNewGDMHelp = `initialise a new test sous GDM
 
-usage: sous newgdm
+usage: sous newgdm -dir dir -clusters a,b,c -singularities a[,b[,c]] -docker-reg somereg
 `
 
 // Help returns the help string for this command
@@ -35,8 +40,10 @@ func (si *SousNewGDM) Help() string { return sousNewGDMHelp }
 
 // AddFlags adds the flags for sous init.
 func (si *SousNewGDM) AddFlags(fs *flag.FlagSet) {
-	fs.StringVar(&si.flags.CheckoutDir, "checkout-dir", "", "dir to create live checkout (defaults to config.StateLocation)")
-	fs.StringVar(&si.flags.RemoteDir, "remote-dir", "", "dir to create git remote")
+	fs.StringVar(&si.flags.CheckoutDir, "dir", "", "dir to create gdm")
+	fs.StringVar(&si.flags.Clusters, "clusters", "", "comma-separated cluster names")
+	fs.StringVar(&si.flags.SingularityURL, "singularity", "", "singularity URL")
+	fs.StringVar(&si.flags.DockerRegURL, "docker-reg", "", "docker registry URL")
 }
 
 // RegisterOn adds flag sets for sous init to the dependency injector.
@@ -45,36 +52,42 @@ func (si *SousNewGDM) RegisterOn(psy Addable) {
 	psy.Add(&config.DeployFilterFlags{})
 }
 
-// Execute fulfills the cmdr.Executor interface
+// Execute fulfils the cmdr.Executor interface
 func (si *SousNewGDM) Execute(args []string) cmdr.Result {
 
-	checkoutDir := func() string {
-		if si.flags.CheckoutDir != "" {
-			return si.flags.CheckoutDir
+	requireFlag := func(name, val string) error {
+		if val != "" {
+			return nil
 		}
-		return si.Config.StateLocation
-	}()
+		return fmt.Errorf("must specify flag -%s", name)
+	}
 
-	dsm := storage.NewDiskStateManager(checkoutDir)
+	flagErr := firsterr.Set(
+		func(e *error) { *e = requireFlag("dir", si.flags.CheckoutDir) },
+		func(e *error) { *e = requireFlag("clusters", si.flags.Clusters) },
+		func(e *error) { *e = requireFlag("singularity", si.flags.SingularityURL) },
+		func(e *error) { *e = requireFlag("docker-reg", si.flags.DockerRegURL) },
+	)
+	if flagErr != nil {
+		return cmdr.EnsureErrorResult(flagErr)
+	}
+
+	dsm := storage.NewDiskStateManager(si.flags.CheckoutDir)
 
 	state := sous.NewState()
-	state.Defs.Clusters = sous.Clusters{
-		"cluster1": &sous.Cluster{
-			Name:              "cluster1",
+	state.Defs.DockerRepo = si.flags.DockerRegURL
+	state.Defs.Clusters = sous.Clusters{}
+	for _, c := range strings.Split(si.flags.Clusters, ",") {
+		state.Defs.Clusters[c] = &sous.Cluster{
+			Name:              c,
 			Kind:              "singularity",
-			BaseURL:           "??",
+			BaseURL:           si.flags.SingularityURL,
 			AllowedAdvisories: sous.VeryPermissiveAdvisories().Strings(),
-		},
-		"cluster2": &sous.Cluster{
-			Name:              "cluster2",
-			Kind:              "singularity",
-			BaseURL:           "??",
-			AllowedAdvisories: sous.VeryPermissiveAdvisories().Strings(),
-		},
+		}
 	}
 
 	if err := dsm.WriteState(state, si.User); err != nil {
 		return cmdr.EnsureErrorResult(err)
 	}
-	return cmdr.Successf("State initialized at %s", checkoutDir)
+	return cmdr.Successf("State initialized at %s", si.flags.CheckoutDir)
 }
