7dbd1dbdb565cdfa577e187b334d40720c785c4e
Sam Salisbury <samsalisbury@gmail.com>
2018-02-21 16:01:17 +0000

    (wip) server: put single deployment

--- server/handle_single_deployment.go
+++ server/handle_single_deployment.go
@@ -3,15 +3,28 @@ package server
 import sous "github.com/opentable/sous/lib"
 
 type (
+	// PUTSingleDeploymentHandler handles manifests containing single deployment
+	// specs. See Exchange method for more details.
 	PUTSingleDeploymentHandler struct {
-		DeploymentID      sous.DeploymentID
-		DeploymentIDError error
-		Manifest          sous.Manifest
-		ManifestError     error
+		DeploymentID    sous.DeploymentID
+		DeploymentIDErr error
+		Body            *singleDeploymentBody
+		BodyErr         error
 	}
-	singleDeploymentResponse struct{}
+
+	// singleDeploymentBody is the response struct returned from handlers
+	// of HTTP methods of a SingleDeploymentResource.
+	singleDeploymentBody struct {
+		DeploymentID   sous.DeploymentID
+		DeploySpec     sous.DeploySpec
+		ManifestHeader sous.Manifest
+	}
 )
 
+// Exchange triggers a deployment action when receiving
+// a Manifest containing a deployment matching DeploymentID that differs
+// from the current actual deployment set. It first writes the new
+// deployment spec to the GDM.
 func (psd *PUTSingleDeploymentHandler) Exchange() (interface{}, int) {
-	return singleDeploymentResponse{}, 404
+	return nil, 404
 }
--- server/handle_single_deployment_test.go
+++ server/handle_single_deployment_test.go
@@ -4,27 +4,103 @@ import (
 	"testing"
 
 	sous "github.com/opentable/sous/lib"
+	"github.com/samsalisbury/semv"
 )
 
 func TestPUTSingleDeploymentHandler_Exchange(t *testing.T) {
 
-	psd := PUTSingleDeploymentHandler{
-		DeploymentID:      sous.DeploymentID{},
-		DeploymentIDError: nil,
-		Manifest:          sous.Manifest{},
-		ManifestError:     nil,
+	makeSingleDeploymentBody := func(repo, cluster, flavor string) *singleDeploymentBody {
+		manifest := sous.Manifest{
+			Source: sous.SourceLocation{
+				Repo: "",
+				Dir:  "",
+			},
+			Flavor: "",
+			Owners: nil,
+			Kind:   "",
+			Deployments: map[string]sous.DeploySpec{
+				"": {
+					DeployConfig: sous.DeployConfig{
+						Resources: map[string]string{
+							"": "",
+						},
+						Metadata: map[string]string{
+							"": "",
+						},
+						Env: map[string]string{
+							"": "",
+						},
+						NumInstances: 0,
+						Volumes:      nil,
+						Startup: sous.Startup{
+							SkipCheck:                 false,
+							ConnectDelay:              0,
+							Timeout:                   0,
+							ConnectInterval:           0,
+							CheckReadyProtocol:        "",
+							CheckReadyURIPath:         "",
+							CheckReadyPortIndex:       0,
+							CheckReadyFailureStatuses: nil,
+							CheckReadyURITimeout:      0,
+							CheckReadyInterval:        0,
+							CheckReadyRetries:         0,
+						},
+						Schedule: "",
+					},
+					Version: semv.MustParse("1"),
+				},
+			},
+		}
+		return &singleDeploymentBody{
+			ManifestHeader: manifest,
+			DeploymentID:   sous.DeploymentID{},
+			DeploySpec:     sous.DeploySpec{},
+		}
 	}
 
-	body, gotStatus := psd.Exchange()
-
-	got, ok := body.(singleDeploymentResponse)
-	if !ok {
-		t.Fatalf("got a %T; want a %T", body, got)
+	testCases := []struct {
+		// BodyAndID is a function that generates a body and an ID.
+		// We expect that if response.DeploymentID == id and the server is
+		// configured to service requests from the corresponding cluster,
+		// the GDM should be updated and a new R11n enqueued.
+		//
+		// The body is sent as the PUT body of the request.
+		// We expect that the same body is returned on success.
+		BodyAndID func() (*singleDeploymentBody, sous.DeploymentID)
+		// WantStatus is the expected HTTP status for this request.
+		WantStatus int
+	}{
+		{
+			BodyAndID: func() (*singleDeploymentBody, sous.DeploymentID) {
+				response := makeSingleDeploymentBody("github.com/user1/repo1", "cluster1", "flavor1")
+				return response, response.DeploymentID
+			},
+		},
 	}
 
-	wantStatus := 404
-	if gotStatus != wantStatus {
-		t.Errorf("got status %d; want %d", gotStatus, wantStatus)
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+
+			sent, did := tc.BodyAndID()
+
+			psd := PUTSingleDeploymentHandler{
+				DeploymentID: did,
+				Body:         sent,
+			}
+
+			received, gotStatus := psd.Exchange()
+
+			got, ok := received.(*singleDeploymentBody)
+
+			if !ok {
+				t.Fatalf("got a %T; want a %T", received, got)
+			}
+
+			wantStatus := 404
+			if gotStatus != wantStatus {
+				t.Errorf("got status %d; want %d", gotStatus, wantStatus)
+			}
+		})
 	}
 
 }
