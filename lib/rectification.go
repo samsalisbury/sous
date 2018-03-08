293be833cf47938cd17ffe3578a64487498597ea
Sam Salisbury <samsalisbury@gmail.com>
2018-03-08 14:39:54 +0000

    lib: bandage close of closed channel
    - The close(done) was panicking occasionally even though I cannot
      find any other reference to that channel.
    - Could be a bug in sync.Once or something weirder (is the once being
      copied in a way that breaks the pattern somewhere?).

--- lib/rectification.go
+++ lib/rectification.go
@@ -27,7 +27,20 @@ func NewRectification(dp DeployablePair) *Rectification {
 func (r *Rectification) Begin(d Deployer) {
 	r.once.Do(func() {
 		r.Resolution = d.Rectify(&r.Pair)
-		close(r.done)
+		// TODO SS: This select statement is a bandage around the problem
+		// that somehow this channel is being closed before reaching the line
+		// below. I doubt it's a bug in sync.Once (though that should be
+		// eliminated). We should figure out the cause and remove this select.
+	CLOSE_CH:
+		select {
+		default:
+			close(r.done)
+		case _, open := <-r.done:
+			if open {
+				goto CLOSE_CH
+			}
+			// Already closed.
+		}
 	})
 }
 
