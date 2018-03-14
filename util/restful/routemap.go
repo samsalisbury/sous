d2ef74e02bb3dbe518a478fad06e9ff99312fd75
lachlanh <lhespe@opentable.com>
2018-03-13 17:22:58 -0700

    fix comment

--- util/restful/routemap.go
+++ util/restful/routemap.go
@@ -235,7 +235,7 @@ func (rm RouteMap) URIFor(name string, pathParams map[string]string, kvs ...KV) 
 	return "/" + strings.Join(pathParts, "/") + query, nil
 }
 
-// FQDN of the URIFor prepends the hostname.
+// FullURIFor returns the hostname with the uri appended
 func (rm RouteMap) FullURIFor(hostName string, name string, pathParams map[string]string, kvs ...KV) (string, error) {
 	uri, err := rm.URIFor(name, pathParams, kvs...)
 	return hostName + uri, err
