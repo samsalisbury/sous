1d4d732360e83cdeacef7d05c9fcbda8521e8b14
Jim Castillo <jcastillo@gmail.com>
2018-03-26 11:51:35 -0700

    add SOUS_CLIENT_DEBUG

--- test/smoke/newdeploy_test.go
+++ test/smoke/newdeploy_test.go
@@ -139,9 +139,15 @@ func (c *TestClient) Configure(server, dockerReg string) error {
 		},
 	}
 	conf.PollIntervalForClient = 600
-	//conf.Logging.Basic.Level = "ExtraDebug1"
-	//conf.Logging.Basic.DisableConsole = false
-	//conf.Logging.Basic.ExtraConsole = true
+
+	clientDebug := os.Getenv("SOUS_CLIENT_DEBUG") == "true"
+
+	if clientDebug {
+		conf.Logging.Basic.Level = "ExtraDebug1"
+		conf.Logging.Basic.DisableConsole = false
+		conf.Logging.Basic.ExtraConsole = true
+	}
+
 	y, err := yaml.Marshal(conf)
 	if err != nil {
 		return err
@@ -596,7 +602,16 @@ CMD if [ -z "$T" ]; then T=2; fi; echo -n "Sleeping ${T}s..."; sleep $T; echo "D
 		t.Fatal(err)
 	}
 
-	if _, err := sous.Run(t, "newdeploy", "-cluster", "cluster1", "-tag", "1.2.3"); err != nil {
+	clientDebug := os.Getenv("SOUS_CLIENT_DEBUG") == "true"
+	commands := []string{}
+
+	if clientDebug {
+		commands = []string{"newdeploy", "-d", "-cluster", "cluster1", "-tag", "1.2.3"}
+	} else {
+		commands = []string{"newdeploy", "-cluster", "cluster1", "-tag", "1.2.3"}
+	}
+
+	if _, err := sous.Run(t, commands...); err != nil {
 		t.Fatal(err)
 	}
 
