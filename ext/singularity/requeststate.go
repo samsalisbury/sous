ca6f470ec51dee2be88bc82a01cf554a108d0d73
Judson <nyarly@gmail.com>
2018-03-26 13:15:20 -0700

    merging

--- ext/singularity/requeststate.go
+++ ext/singularity/requeststate.go
@@ -26,33 +26,11 @@ func (r *deployer) Status(reg sous.Registry, clusters sous.Clusters, pair *sous.
 		return nil, errors.Errorf("No cluster found for %q. Known are: %q.", clusterName, clusters.Names())
 	}
 
+	depID := computeDeployID(pair.Post)
 	client := r.buildSingClient(url)
-	log.Println("Watching pending deployments for deploy ID:", pair.Post.SchedulerDID)
+	log.Println("Watching pending deployments for deploy ID:", depID)
 
 	counter := 0
-
-WAIT_FOR_NOT_PENDING:
-
-	pending, err := client.GetPendingDeploys()
-
-	if err != nil {
-		return nil, malformedResponse{"Getting pending deploys:" + err.Error()}
-	}
-	pds := make([]string, len(pending))
-	for i, p := range pending {
-		pds[i] = p.DeployMarker.DeployId
-	}
-
-	log.Println("Watching pending deployments for deploy ID:", pair.Post.SchedulerDID)
-	log.Printf("Counter: %d - There are %d pending deploys: %s", counter, len(pending), strings.Join(pds, ", "))
-
-	for _, p := range pending {
-		if p.DeployMarker.DeployId == pair.Post.SchedulerDID && counter < 600 {
-			counter = counter + 1
-			time.Sleep(2 * time.Second) //this is what OTPL does
-			goto WAIT_FOR_NOT_PENDING
-		}
-	}
 
 	reqParent, err := client.GetRequest(reqID, false) //don't use the web cache
 	if err != nil {
@@ -69,10 +47,34 @@ WAIT_FOR_NOT_PENDING:
 
 	tgt.SchedulerURL = fmt.Sprintf("http://%s/request/%s", url, reqID)
 
+	pending, err := client.GetPendingDeploys()
+
+	if err != nil {
+		return nil, malformedResponse{"Getting pending deploys:" + err.Error()}
+	}
+	pds := make([]string, len(pending))
+	for i, p := range pending {
+		pds[i] = p.DeployMarker.DeployId
+	}
+
+	log.Println("Watching pending deployments for deploy ID:", depID)
+	log.Printf("Counter: %d - There are %d pending deploys: %s", counter, len(pending), strings.Join(pds, ", "))
+
+	for _, p := range pending {
+		if p.DeployMarker.DeployId == depID && counter < 600 {
+			counter = counter + 1
+			time.Sleep(2 * time.Second) //this is what OTPL does
+			goto WAIT_FOR_NOT_PENDING
+		}
+	}
+
 	return &tgt, errors.Wrapf(err, "getting request state")
 }
 
 func (r *deployer) getRequestID(d *sous.Deployable) (string, error) {
 	// TODO: add a cache of known Deployables to their Requests (and current state...)
 	return computeRequestID(d)
+}
+
+func (r *deployer) checkPendingList(d *sous.Deployable) bool {
 }
