95e5ec519ee65075429425b6f3345c11244dcce6
Sam Salisbury <samsalisbury@gmail.com>
2018-03-28 16:07:30 -0700

    test/smoke: cleanup

--- test/smoke/newdeploy_test.go
+++ test/smoke/newdeploy_test.go
@@ -4,7 +4,6 @@ package smoke
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -18,65 +17,50 @@ CMD if [ -z "$T" ]; then T=2; fi; echo -n "Sleeping ${T}s..."; sleep $T; echo "D
 func TestSousNewdeploy(t *testing.T) {
 	f := setupEnv(t, "TestSousNewdeploy")
 
+	// Setup project git repo.
 	projectDir := makeGitRepo(t, f.BaseDir, "projects/project1", GitRepoSpec{
 		UserName:  "Sous User 1",
 		UserEmail: "sous-user1@example.com",
 		OriginURL: "git@github.com:opentable/bogus/repo1",
 	})
-
 	makeFileString(t, projectDir, "Dockerfile", simpleServer)
 	mustDoCMD(t, projectDir, "git", "add", "Dockerfile")
-	mustDoCMD(t, projectDir, "git", "commit", "-am", "Add Dockerfile")
+	mustDoCMD(t, projectDir, "git", "commit", "-m", "Add Dockerfile")
 
 	sous := f.Client
 
-	sous.MustRun(t, "version")
+	// Dump sous version & config.
+	t.Logf("Sous version: %s", sous.MustRun(t, "version"))
+	sous.MustRun("config")
+
+	// cd into project dir
+	sous.Dir = projectDir
 
 	// sous init
-	sous.Dir = projectDir
 	sous.MustRun(t, "init")
 
 	// sous manifest get > manifest
-	manifestGetCmd := sous.Cmd(t, "manifest", "get")
-	manifestBytes, err := manifestGetCmd.Output()
-	if err != nil {
-		//output, err := manifestGetCmd.CombinedOutput()
-		//		if err != nil {
-		t.Fatalf("sous manifest get failed: %s; output:\n%s", err, string(manifestBytes))
-		//}
-		//t.Fatalf("sous manifest get weirdly failed then succeeded")
-	}
-
-	fmt.Println(string(manifestBytes))
+	manifest := sous.MustRun(t, "manifest", "get")
 
 	// edit manifest
-	manifest := strings.Replace(string(manifestBytes), "NumInstances: 0", "NumInstances: 1", -1)
-
-	fmt.Print(manifest)
+	manifest = strings.Replace(manifest, "NumInstances: 0", "NumInstances: 1", -1)
 
 	// sous manifest set < manifest
 	manifestSetCmd := sous.Cmd(t, "manifest", "set")
 	manifestSetCmd.Stdin = ioutil.NopCloser(bytes.NewReader([]byte(manifest)))
-
 	if out, err := manifestSetCmd.CombinedOutput(); err != nil {
 		t.Fatalf("manifest set failed: %s; output:\n%s", err, out)
 	}
 
-	if _, err := sous.Run(t, "build", "-tag", "1.2.3"); err != nil {
-		t.Fatal(err)
-	}
+	// sous build
+	sous.MustRun(t, "build", "-tag", "1.2.3")
 
+	// sous newdeploy
 	clientDebug := os.Getenv("SOUS_CLIENT_DEBUG") == "true"
-	commands := []string{}
-
+	args := []string{"-cluster", "cluster1", "-tag", "1.2.3"}
 	if clientDebug {
-		commands = []string{"newdeploy", "-d", "-cluster", "cluster1", "-tag", "1.2.3"}
-	} else {
-		commands = []string{"newdeploy", "-cluster", "cluster1", "-tag", "1.2.3"}
+		args = append([]string{"-d"}, args...)
 	}
 
-	if _, err := sous.Run(t, commands...); err != nil {
-		t.Fatal(err)
-	}
-
+	sous.MustRun(t, append([]string{"newdeploy"}, args...))
 }
