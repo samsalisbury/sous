8ccc7e55538c269c153e1141b1c92f911487de77
Jim Castillo <jcastillo@gmail.com>
2018-04-04 14:11:02 -0700

    update test

--- util/logging/messages/generalmsg_test.go
+++ util/logging/messages/generalmsg_test.go
@@ -118,11 +118,12 @@ func TestReportLogFieldsMessage_Submessage(t *testing.T) {
 		map[string]interface{}{
 			"@loglov3-otl":        "sous-generic-v1",
 			"call-stack-function": "github.com/opentable/sous/util/logging/messages.TestReportLogFieldsMessage_Submessage",
-			"sous-fields":         "",
+			"sous-fields":         "field,testSubmessage",
 			"sous-id-values":      "",
 			"sous-ids":            "",
-			"sous-types":          "",
+			"sous-types":          "testSubmessage",
 			"test-field":          42,
+			"json-value":          "{\"message\":{\"array\":[\"(messages.testSubmessage) {\\n field: (int) 42\\n}\\n\"]}}",
 		},
 	)
 }
