03b40ca2feb00ca837bbc756f7866a7154b88a79
Sam Salisbury <samsalisbury@gmail.com>
2018-04-17 17:38:53 +0100

    test/smoke: Singularity.(P|Unp)ause

--- test/smoke/testsingularity_utils_test.go
+++ test/smoke/testsingularity_utils_test.go
@@ -1,11 +1,16 @@
 package smoke
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"testing"
 	"time"
 
 	sing "github.com/opentable/go-singularity"
+	"github.com/opentable/go-singularity/dtos"
+	"github.com/opentable/sous/ext/singularity"
+	sous "github.com/opentable/sous/lib"
 )
 
 type Singularity struct {
@@ -15,6 +20,106 @@ type Singularity struct {
 
 func NewSingularity(baseURL string) *Singularity {
 	return &Singularity{URL: baseURL, client: sing.NewClient(baseURL)}
+}
+
+func (s *Singularity) PauseRequestForDeployment(t *testing.T, did sous.DeploymentID) {
+	reqID, err := singularity.MakeRequestID(did)
+	if err != nil {
+		t.Fatalf("making singularity request ID: %s", err)
+	}
+
+	if _, err := s.client.Pause(reqID, nil); err != nil {
+		t.Fatal(err)
+	}
+
+	var depID string
+	waitFor(t, "paused status", 30*time.Second, 2*time.Second, func() error {
+		req, err := s.client.GetRequest(reqID, false)
+		if req.ActiveDeploy != nil && req.ActiveDeploy.Id != "" {
+			depID = req.ActiveDeploy.Id
+		}
+		if err != nil {
+			return err
+		}
+		if req.State != dtos.SingularityRequestParentRequestStatePAUSED {
+			return fmt.Errorf("status is %s", req.State)
+		}
+		return nil
+	})
+
+	waitFor(t, "tasks to stop", 30*time.Second, 2*time.Second, func() error {
+		for {
+			h, err := s.client.GetActiveDeployTasks(reqID, depID)
+			if err != nil {
+				return err
+			}
+			if len(h) != 0 {
+				return fmt.Errorf("%d tasks running", len(h))
+			}
+			return nil
+		}
+	})
+}
+
+func (s *Singularity) UnpauseRequestForDeployment(t *testing.T, did sous.DeploymentID) {
+	reqID, err := singularity.MakeRequestID(did)
+	if err != nil {
+		t.Fatalf("making singularity request ID: %s", err)
+	}
+
+	if _, err := s.client.Unpause(reqID, nil); err != nil {
+		t.Fatal(err)
+	}
+
+	waitFor(t, "not paused status", 30*time.Second, 2*time.Second, func() error {
+		req, err := s.client.GetRequest(reqID, false)
+		if err != nil {
+			return err
+		}
+		if req.State == dtos.SingularityRequestParentRequestStatePAUSED {
+			return fmt.Errorf("status is %s", req.State)
+		}
+		return nil
+	})
+}
+
+func waitFor(t *testing.T, what string, timeout, interval time.Duration, f func() error) {
+	t.Helper()
+	fmt.Fprintf(os.Stderr, "waitFor: Waiting for %s...\n", what)
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	select {
+	case <-time.After(timeout):
+		t.Fatalf("timed out waiting for %s after %s", what, timeout)
+	case <-(func() <-chan struct{} {
+		c := make(chan struct{})
+		go func() {
+			defer close(c)
+			for {
+				err := func() error {
+					select {
+					case <-ticker.C:
+						return fmt.Errorf("timed out after %s", interval)
+					case err := <-(func() <-chan error {
+						c := make(chan error)
+						go func() { c <- f() }()
+						return c
+					}()):
+						return err
+					}
+				}()
+				if err != nil {
+					// Log direct to stderr for live updates.
+					fmt.Fprintf(os.Stderr, "waitFor: Waiting for %s: %s\n", what, err)
+					<-ticker.C
+					continue
+				}
+				break
+			}
+		}()
+		return c
+	}()):
+	}
 }
 
 func (s *Singularity) Reset(t *testing.T) {
