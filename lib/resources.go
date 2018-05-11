18b33fd96ac35c4eab838e1b94ebafb21dbb7913
Jim Castillo <jcastillo@gmail.com>
2018-05-11 14:34:53 -0700

    remove logging from resources.go

--- lib/resources.go
+++ lib/resources.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"math"
 	"strconv"
-
-	"github.com/opentable/sous/util/logging"
 )
 
 type (
@@ -104,119 +102,51 @@ func (r Resources) validateField(name, def string) Flaw {
 
 // Cpus returns the number of CPUs.
 func (r Resources) Cpus() float64 {
-	cpuStr, present := r["cpus"]
+	cpuStr := r["cpus"]
 	cpus, err := strconv.ParseFloat(cpuStr, 64)
 	if err != nil {
 		cpus = 0.1
-		if present {
-			reportResourceMessage(fmt.Sprintf("Could not parse value: '%s' for cpus as a float, using default: %f", cpuStr, cpus), r)
-		} else {
-			reportDebugResourceMessage(fmt.Sprintf("Using default value for cpus: %f.", cpus), r)
-		}
 	}
 	return cpus
 }
 
 // Memory returns memory in MB.
 func (r Resources) Memory() float64 {
-	memStr, present := r["memory"]
+	memStr := r["memory"]
 	memory, err := strconv.ParseFloat(memStr, 64)
 	if err != nil {
 		memory = 100
-		if present {
-			reportResourceMessage(fmt.Sprintf("Could not parse value: '%s' for memory as an int, using default: %f", memStr, memory), r)
-		} else {
-			reportDebugResourceMessage(fmt.Sprintf("Using default value for memory: %f.", memory), r)
-		}
 	}
 	return memory
 }
 
 // Ports returns the number of ports required.
 func (r Resources) Ports() int32 {
-	portStr, present := r["ports"]
+	portStr := r["ports"]
 	ports, err := strconv.ParseInt(portStr, 10, 32)
 	if err != nil {
 		ports = 1
-		if present {
-			reportResourceMessage(fmt.Sprintf("Could not parse value: '%s' for ports as a int, using default: %d", portStr, ports), r)
-		} else {
-			reportDebugResourceMessage(fmt.Sprintf("Using default value for ports: %d", ports), r)
-		}
 	}
 	return int32(ports)
 }
 
 // Equal checks equivalence between resource maps
 func (r Resources) Equal(o Resources) bool {
-	reportDebugResourceMessage(fmt.Sprintf("Comparing resources: %+ v ?= %+ v", r, o), r)
 	if len(r) != len(o) {
-		reportDebugResourceMessage("Lengths differ", r)
 		return false
 	}
 
 	if r.Ports() != o.Ports() {
-		reportDebugResourceMessage("Ports differ", r)
 		return false
 	}
 
 	if math.Abs(r.Cpus()-o.Cpus()) > 0.001 {
-		reportDebugResourceMessage("Cpus differ", r)
 		return false
 	}
 
 	if math.Abs(r.Memory()-o.Memory()) > 0.001 {
-		reportDebugResourceMessage("Memory differ", r)
 		return false
 	}
 
 	return true
-}
-
-type resourceMessage struct {
-	logging.CallerInfo
-	msg        string
-	ports      int32
-	cpus       float64
-	memory     float64
-	isDebugMsg bool
-}
-
-func reportDebugResourceMessage(msg string, r Resources) {
-	reportResourceMessage(msg, r, true)
-}
-
-func reportResourceMessage(msg string, r Resources, debug ...bool) {
-	log := *(logging.SilentLogSet().Child("Resources").(*logging.LogSet))
-	lvl := logging.WarningLevel
-	if len(debug) > 0 && debug[0] {
-		lvl = logging.DebugLevel
-	}
-
-	//not going to call Ports/Cpus/Memory to get values since those functions actually call reportResourceMessage
-	var ports int32
-	if portStr, present := r["ports"]; present {
-		ports64, _ := strconv.ParseInt(portStr, 10, 32)
-		ports = int32(ports64)
-	}
-	var memory logging.MemResourceField
-	if memStr, present := r["memory"]; present {
-		mf, _ := strconv.ParseFloat(memStr, 64)
-		memory = logging.MemResourceField(mf)
-	}
-	var cpus logging.CPUResourceField
-	if cpuStr, present := r["cpus"]; present {
-		cf, _ := strconv.ParseFloat(cpuStr, 64)
-		cpus = logging.CPUResourceField(cf)
-	}
-
-	logging.Deliver(log,
-		logging.SousGenericV1,
-		lvl,
-		logging.GetCallerInfo(logging.NotHere()),
-		logging.MessageField(fmt.Sprintf("%s: ports %v, cpus %v memory %v", msg, ports, cpus, memory)),
-		logging.KV(logging.SousResourcePorts, ports),
-		memory,
-		cpus,
-	)
 }
