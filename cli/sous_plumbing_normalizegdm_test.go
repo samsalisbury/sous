8ebc1ab1b0065bc901357ab8a7911cca5d611b98
Jim Castillo <jcastillo@gmail.com>
2018-05-18 14:17:56 -0700

    add more tests

--- cli/sous_plumbing_normalizegdm_test.go
+++ cli/sous_plumbing_normalizegdm_test.go
@@ -3,6 +3,9 @@ package cli
 import (
 	"testing"
 
+	"github.com/opentable/sous/config"
+	"github.com/opentable/sous/graph"
+	"github.com/opentable/sous/util/cmdr"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,5 +16,22 @@ func TestHelp(t *testing.T) {
 	help := p.Help()
 
 	require.True(t, len(help) > 0)
+
+}
+
+func TestExecute(t *testing.T) {
+
+	gr := graph.DefaultTestGraph()
+
+	c := &config.Config{Server: "", StateLocation: "/tmp/sous"}
+
+	require := require.New(t)
+	p := &SousPlumbingNormalizeGDM{SousGraph: gr, LocalSousConfig: graph.LocalSousConfig{Config: c}}
+
+	args := []string{"", ""}
+
+	result := p.Execute(args)
+
+	require.IsType(cmdr.UnknownErr{}, result)
 
 }
