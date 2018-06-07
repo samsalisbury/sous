88d5f814b5861a1747b2ca8552c0d808036ab60d
Sam Salisbury <samsalisbury@gmail.com>
2018-06-07 13:14:04 +0100

    cli: sous manifest edit works on macOS
    - For some reason, maintaining the file reference and seeking back
      did not allow changes just written by vim to be seen by the next read.
    - Not certain if this is peculiar to macOS (APFS maybe?) or whether this
      is expected bahaviour; but closing and re-opening the file handle
      certainly fixes the issue on my machine.
    - I would love to get a test around this behaviour, but really hard to do..

--- cli/sous_manifest_edit.go
+++ cli/sous_manifest_edit.go
@@ -44,20 +44,25 @@ func (sme *SousManifestEdit) Execute(args []string) cmdr.Result {
 		return EnsureErrorResult(err)
 	}
 
-	set, err := sme.SousGraph.GetManifestSet(sme.DeployFilterFlags, &up, file)
-	if err != nil {
-		return EnsureErrorResult(err)
-	}
-
 	if err := get.Do(); err != nil {
 		return EnsureErrorResult(errors.Wrapf(err, "getting manifest into %s", file.Name()))
+	}
+
+	if err := file.Close(); err != nil {
+		return EnsureErrorResult(errors.Wrapf(err, "closing file %s", file.Name()))
 	}
 
 	if err := doEdit(file.Name()); err != nil {
 		return EnsureErrorResult(errors.Wrapf(err, "editing file at %s", file.Name()))
 	}
 
-	if _, err := file.Seek(0, 0); err != nil {
+	file, err = os.Open(file.Name())
+	if err != nil {
+		return EnsureErrorResult(errors.Wrapf(err, "reopening file %s", file.Name()))
+	}
+
+	set, err := sme.SousGraph.GetManifestSet(sme.DeployFilterFlags, &up, file)
+	if err != nil {
 		return EnsureErrorResult(err)
 	}
 
