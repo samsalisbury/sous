6736ee06ccde5d4b8c37576c2cbaf5695a526eba
Sam Salisbury <samsalisbury@gmail.com>
2018-06-08 17:45:36 +0100

    test/smoke: gofmt

--- test/smoke/newdeploy_test.go
+++ test/smoke/newdeploy_test.go
@@ -89,8 +89,8 @@ func TestSousDeploy(t *testing.T) {
 
 					assertActiveStatus(t, f, did)
 					assertSingularityRequestTypeService(t, f, did)
-          assertNonNilHealthCheckOnLatestDeploy(t, f, did)
-        	assertUserOnLatestDeploy(t, f, did)
+					assertNonNilHealthCheckOnLatestDeploy(t, f, did)
+					assertUserOnLatestDeploy(t, f, did)
 				})
 
 				t.Run("zero-instances", func(t *testing.T) {
