0dfd2fe878927b01bd8e1d41c6fa661fcb55a8d0
Sam Salisbury <samsalisbury@gmail.com>
2018-06-08 15:49:00 +0100

    test/smoke: print summary synchronisation

--- test/smoke/newdeploy_test.go
+++ test/smoke/newdeploy_test.go
@@ -63,164 +63,170 @@ func TestSousDeploy(t *testing.T) {
 		NumFreeAddrs: 128,
 	})
 
-	for _, fixtureConfig := range fixtureConfigs {
-		t.Run(fixtureConfig.Desc(), func(t *testing.T) {
-			t.Parallel()
-			deployCommand := "deploy"
-			t.Run(deployCommand, func(t *testing.T) {
+	// This outer Run call does not return until all inner parallel tests have
+	// finished. This allows the final pf.PrintSummary call to happen only after
+	// all tests have concluded.
+	t.Run("_", func(t *testing.T) {
+
+		for _, fixtureConfig := range fixtureConfigs {
+			t.Run(fixtureConfig.Desc(), func(t *testing.T) {
 				t.Parallel()
+				deployCommand := "deploy"
+				t.Run(deployCommand, func(t *testing.T) {
+					t.Parallel()
 
-				t.Run("simple", func(t *testing.T) {
-					f := pf.NewIsolatedFixture(t, fixtureConfig)
-					defer f.ReportSuccess(t)
-					client := setupProject(t, f, simpleServer)
-					client.MustRun(t, "init", nil, "-kind", "http-service")
-					client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst1)
-					client.MustRun(t, "build", nil, "-tag", "1.2.3")
-					client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
+					t.Run("simple", func(t *testing.T) {
+						f := pf.NewIsolatedFixture(t, fixtureConfig)
+						defer f.ReportSuccess(t)
+						client := setupProject(t, f, simpleServer)
+						client.MustRun(t, "init", nil, "-kind", "http-service")
+						client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst1)
+						client.MustRun(t, "build", nil, "-tag", "1.2.3")
+						client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
 
-					did := sous.DeploymentID{
-						ManifestID: sous.ManifestID{
-							Source: sous.SourceLocation{
-								Repo: "github.com/user1/repo1",
+						did := sous.DeploymentID{
+							ManifestID: sous.ManifestID{
+								Source: sous.SourceLocation{
+									Repo: "github.com/user1/repo1",
+								},
 							},
-						},
-						Cluster: "cluster1",
-					}
+							Cluster: "cluster1",
+						}
 
-					assertActiveStatus(t, f, did)
-					assertSingularityRequestTypeService(t, f, did)
-					assertNonNilHealthCheckOnLatestDeploy(t, f, did)
-				})
-
-				t.Run("zero-instances", func(t *testing.T) {
-					f := pf.NewIsolatedFixture(t, fixtureConfig)
-					defer f.ReportSuccess(t)
-					client := setupProject(t, f, simpleServer)
-					client.MustRun(t, "init", nil, "-kind", "http-service")
-					client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst0)
-					client.MustRun(t, "build", nil, "-tag", "1.2.3")
-
-					client.MustFail(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
-				})
-
-				t.Run("fails", func(t *testing.T) {
-					f := pf.NewIsolatedFixture(t, fixtureConfig)
-					defer f.ReportSuccess(t)
-					client := setupProject(t, f, failer)
-					client.MustRun(t, "init", nil, "-kind", "http-service")
-					client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst1)
-					client.MustRun(t, "build", nil, "-tag", "1.2.3")
-					client.MustFail(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
-
-					did := sous.DeploymentID{
-						ManifestID: sous.ManifestID{
-							Source: sous.SourceLocation{
-								Repo: "github.com/user1/repo1",
-							},
-						},
-						Cluster: "cluster1",
-					}
-
-					assertActiveStatus(t, f, did)
-					assertSingularityRequestTypeService(t, f, did)
-					assertNonNilHealthCheckOnLatestDeploy(t, f, did)
-				})
-
-				t.Run("flavors", func(t *testing.T) {
-					f := pf.NewIsolatedFixture(t, fixtureConfig)
-					defer f.ReportSuccess(t)
-					client := setupProject(t, f, simpleServer)
-					flavor := "flavor1"
-					flavorFlag := &sousFlags{flavor: flavor}
-					client.MustRun(t, "init", flavorFlag, "-kind", "http-service")
-					client.TransformManifest(t, flavorFlag, setMinimalMemAndCPUNumInst1)
-					client.MustRun(t, "build", nil, "-tag", "1.2.3")
-					client.MustRun(t, deployCommand, flavorFlag, "-cluster", "cluster1", "-tag", "1.2.3")
-
-					did := sous.DeploymentID{
-						ManifestID: sous.ManifestID{
-							Source: sous.SourceLocation{
-								Repo: "github.com/user1/repo1",
-							},
-							Flavor: flavor,
-						},
-						Cluster: "cluster1",
-					}
-
-					assertActiveStatus(t, f, did)
-					assertSingularityRequestTypeService(t, f, did)
-					assertNonNilHealthCheckOnLatestDeploy(t, f, did)
-				})
-
-				t.Run("pause-unpause", func(t *testing.T) {
-					f := pf.NewIsolatedFixture(t, fixtureConfig)
-					defer f.ReportSuccess(t)
-					client := setupProject(t, f, simpleServer)
-					client.MustRun(t, "init", nil, "-kind", "http-service")
-					client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst1)
-					client.MustRun(t, "build", nil, "-tag", "1")
-					client.MustRun(t, "build", nil, "-tag", "2")
-					client.MustRun(t, "build", nil, "-tag", "3")
-					client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1")
-
-					did := sous.DeploymentID{
-						ManifestID: sous.ManifestID{
-							Source: sous.SourceLocation{
-								Repo: "github.com/user1/repo1",
-							},
-						},
-						Cluster: "cluster1",
-					}
-					assertActiveStatus(t, f, did)
-					assertNonNilHealthCheckOnLatestDeploy(t, f, did)
-					assertSingularityRequestTypeService(t, f, did)
-
-					f.Singularity.PauseRequestForDeployment(t, did)
-					client.MustFail(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "2")
-					f.Singularity.UnpauseRequestForDeployment(t, did)
-
-					f.KnownToFailHere(t)
-
-					client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "3")
-					assertActiveStatus(t, f, did)
-				})
-
-				t.Run("scheduled", func(t *testing.T) {
-					f := pf.NewIsolatedFixture(t, fixtureConfig)
-					defer f.ReportSuccess(t)
-					client := setupProject(t, f, sleeper)
-					client.MustRun(t, "init", nil, "-kind", "scheduled")
-					client.TransformManifest(t, nil, func(m sous.Manifest) sous.Manifest {
-						clusterName := "cluster1" + f.ClusterSuffix
-						d := m.Deployments[clusterName]
-						d.NumInstances = 1
-						d.Schedule = "*/5 * * * *"
-						m.Deployments[clusterName] = d
-
-						m.Deployments = setMemAndCPUForAll(m.Deployments)
-
-						return m
+						assertActiveStatus(t, f, did)
+						assertSingularityRequestTypeService(t, f, did)
+						assertNonNilHealthCheckOnLatestDeploy(t, f, did)
 					})
-					client.MustRun(t, "build", nil, "-tag", "1.2.3")
-					client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
 
-					did := sous.DeploymentID{
-						ManifestID: sous.ManifestID{
-							Source: sous.SourceLocation{
-								Repo: "github.com/user1/repo1",
+					t.Run("zero-instances", func(t *testing.T) {
+						f := pf.NewIsolatedFixture(t, fixtureConfig)
+						defer f.ReportSuccess(t)
+						client := setupProject(t, f, simpleServer)
+						client.MustRun(t, "init", nil, "-kind", "http-service")
+						client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst0)
+						client.MustRun(t, "build", nil, "-tag", "1.2.3")
+
+						client.MustFail(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
+					})
+
+					t.Run("fails", func(t *testing.T) {
+						f := pf.NewIsolatedFixture(t, fixtureConfig)
+						defer f.ReportSuccess(t)
+						client := setupProject(t, f, failer)
+						client.MustRun(t, "init", nil, "-kind", "http-service")
+						client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst1)
+						client.MustRun(t, "build", nil, "-tag", "1.2.3")
+						client.MustFail(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
+
+						did := sous.DeploymentID{
+							ManifestID: sous.ManifestID{
+								Source: sous.SourceLocation{
+									Repo: "github.com/user1/repo1",
+								},
 							},
-						},
-						Cluster: "cluster1",
-					}
+							Cluster: "cluster1",
+						}
 
-					assertSingularityRequestTypeScheduled(t, f, did)
-					assertActiveStatus(t, f, did)
-					assertNilHealthCheckOnLatestDeploy(t, f, did)
+						assertActiveStatus(t, f, did)
+						assertSingularityRequestTypeService(t, f, did)
+						assertNonNilHealthCheckOnLatestDeploy(t, f, did)
+					})
+
+					t.Run("flavors", func(t *testing.T) {
+						f := pf.NewIsolatedFixture(t, fixtureConfig)
+						defer f.ReportSuccess(t)
+						client := setupProject(t, f, simpleServer)
+						flavor := "flavor1"
+						flavorFlag := &sousFlags{flavor: flavor}
+						client.MustRun(t, "init", flavorFlag, "-kind", "http-service")
+						client.TransformManifest(t, flavorFlag, setMinimalMemAndCPUNumInst1)
+						client.MustRun(t, "build", nil, "-tag", "1.2.3")
+						client.MustRun(t, deployCommand, flavorFlag, "-cluster", "cluster1", "-tag", "1.2.3")
+
+						did := sous.DeploymentID{
+							ManifestID: sous.ManifestID{
+								Source: sous.SourceLocation{
+									Repo: "github.com/user1/repo1",
+								},
+								Flavor: flavor,
+							},
+							Cluster: "cluster1",
+						}
+
+						assertActiveStatus(t, f, did)
+						assertSingularityRequestTypeService(t, f, did)
+						assertNonNilHealthCheckOnLatestDeploy(t, f, did)
+					})
+
+					t.Run("pause-unpause", func(t *testing.T) {
+						f := pf.NewIsolatedFixture(t, fixtureConfig)
+						defer f.ReportSuccess(t)
+						client := setupProject(t, f, simpleServer)
+						client.MustRun(t, "init", nil, "-kind", "http-service")
+						client.TransformManifest(t, nil, setMinimalMemAndCPUNumInst1)
+						client.MustRun(t, "build", nil, "-tag", "1")
+						client.MustRun(t, "build", nil, "-tag", "2")
+						client.MustRun(t, "build", nil, "-tag", "3")
+						client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1")
+
+						did := sous.DeploymentID{
+							ManifestID: sous.ManifestID{
+								Source: sous.SourceLocation{
+									Repo: "github.com/user1/repo1",
+								},
+							},
+							Cluster: "cluster1",
+						}
+						assertActiveStatus(t, f, did)
+						assertNonNilHealthCheckOnLatestDeploy(t, f, did)
+						assertSingularityRequestTypeService(t, f, did)
+
+						f.Singularity.PauseRequestForDeployment(t, did)
+						client.MustFail(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "2")
+						f.Singularity.UnpauseRequestForDeployment(t, did)
+
+						f.KnownToFailHere(t)
+
+						client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "3")
+						assertActiveStatus(t, f, did)
+					})
+
+					t.Run("scheduled", func(t *testing.T) {
+						f := pf.NewIsolatedFixture(t, fixtureConfig)
+						defer f.ReportSuccess(t)
+						client := setupProject(t, f, sleeper)
+						client.MustRun(t, "init", nil, "-kind", "scheduled")
+						client.TransformManifest(t, nil, func(m sous.Manifest) sous.Manifest {
+							clusterName := "cluster1" + f.ClusterSuffix
+							d := m.Deployments[clusterName]
+							d.NumInstances = 1
+							d.Schedule = "*/5 * * * *"
+							m.Deployments[clusterName] = d
+
+							m.Deployments = setMemAndCPUForAll(m.Deployments)
+
+							return m
+						})
+						client.MustRun(t, "build", nil, "-tag", "1.2.3")
+						client.MustRun(t, deployCommand, nil, "-cluster", "cluster1", "-tag", "1.2.3")
+
+						did := sous.DeploymentID{
+							ManifestID: sous.ManifestID{
+								Source: sous.SourceLocation{
+									Repo: "github.com/user1/repo1",
+								},
+							},
+							Cluster: "cluster1",
+						}
+
+						assertSingularityRequestTypeScheduled(t, f, did)
+						assertActiveStatus(t, f, did)
+						assertNilHealthCheckOnLatestDeploy(t, f, did)
+					})
 				})
 			})
-		})
-	}
+		}
+	})
 
 	pf.PrintSummary(t)
 }
