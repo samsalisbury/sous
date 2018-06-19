0140d87cdaa1a271b1c806f260abafb8b7593ed3
Sam Salisbury <samsalisbury@gmail.com>
2018-06-19 15:59:33 +0100

    test/smoke: Startup.ConnectDelay = 0 by default
    - Keep getting tasks timing out/going overdue, hoping this will help.

--- test/smoke/testmanifesttransforms_test.go
+++ test/smoke/testmanifesttransforms_test.go
@@ -6,8 +6,11 @@ import sous "github.com/opentable/sous/lib"
 
 func setMemAndCPUForAll(ds sous.DeploySpecs) sous.DeploySpecs {
 	for c := range ds {
-		ds[c].Resources["memory"] = "1"
-		ds[c].Resources["cpus"] = "0.001"
+		d := ds[c]
+		d.Resources["memory"] = "1"
+		d.Resources["cpus"] = "0.001"
+		d.Startup.ConnectDelay = 0
+		ds[c] = d
 	}
 	return ds
 }
@@ -17,6 +20,7 @@ func setMinimalMemAndCPUNumInst1(m sous.Manifest) sous.Manifest {
 		d.Resources["memory"] = "1"
 		d.Resources["cpus"] = "0.001"
 		d.NumInstances = 1
+		d.Startup.ConnectDelay = 0
 		return d
 	})
 }
@@ -26,6 +30,7 @@ func setMinimalMemAndCPUNumInst0(m sous.Manifest) sous.Manifest {
 		d.Resources["memory"] = "1"
 		d.Resources["cpus"] = "0.001"
 		d.NumInstances = 0
+		d.Startup.ConnectDelay = 0
 		return d
 	})
 }
