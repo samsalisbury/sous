428d9671bda6133eb8f6339c7e6d1b2536ca4bb4
Sam Salisbury <samsalisbury@gmail.com>
2018-06-21 21:48:42 +0100

    singularity: centralise mv SingularityRequestID
    - Less spaghetti..

--- ext/singularity/deployer.go
+++ ext/singularity/deployer.go
@@ -249,18 +249,29 @@ func (r *deployer) RectifySingleModification(pair *sous.DeployablePair) (err err
 		desiredReqID = currentReqID
 	}
 
-	reportDeployerMessage("Operating on request", pair, diffs, data, nil, logging.ExtraDebug1Level, r.log)
-	if changesReq(pair) {
-		reportDeployerMessage("Updating request", pair, diffs, data, nil, logging.DebugLevel, r.log)
-		if err := r.Client.PostRequest(*pair.Post, desiredReqID); err != nil {
-			return err
-		}
-	} else if desiredReqID != currentReqID {
+	if desiredReqID != currentReqID {
 		// NOTE: This message is WarningLevel whilst the feature is new.
 		// TODO SS: Turn this down to debug level once we're happy with it.
 		m := fmt.Sprintf("Creating request %q to replace %q", desiredReqID, currentReqID)
 		reportDeployerMessage(m, pair, diffs, data, nil, logging.WarningLevel, r.log)
 
+		if err := r.Client.PostRequest(*pair.Post, desiredReqID); err != nil {
+			return err
+		}
+
+		reportDeployerMessage("Deploying", pair, diffs, data, nil, logging.DebugLevel, r.log)
+		depID := computeDeployIDFromUUID(pair.Post, pair.UUID)
+		if err := r.Client.Deploy(*pair.Post, desiredReqID, depID); err != nil {
+			return err
+		}
+		// Remove the old request ID only once the new deployment is complete.
+		m = fmt.Sprintf("Renamed to %q", desiredReqID)
+		return r.Client.DeleteRequest(pair.Post.Deployment.Cluster.BaseURL, currentReqID, m)
+	}
+
+	reportDeployerMessage("Operating on request", pair, diffs, data, nil, logging.ExtraDebug1Level, r.log)
+	if changesReq(pair) {
+		reportDeployerMessage("Updating request", pair, diffs, data, nil, logging.DebugLevel, r.log)
 		if err := r.Client.PostRequest(*pair.Post, desiredReqID); err != nil {
 			return err
 		}
@@ -276,14 +287,6 @@ func (r *deployer) RectifySingleModification(pair *sous.DeployablePair) (err err
 		}
 	} else {
 		reportDeployerMessage("No change to Singularity deployment required", pair, diffs, data, nil, logging.DebugLevel, r.log)
-	}
-
-	// Remove the old request ID only once the new deployment is complete.
-	if desiredReqID != currentReqID {
-		m := fmt.Sprintf("Renamed to %q by Sous", desiredReqID)
-		if err := r.Client.DeleteRequest(pair.Post.Deployment.Cluster.BaseURL, currentReqID, m); err != nil {
-			return err
-		}
 	}
 
 	return nil
