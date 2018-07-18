da73a7e32f970f5523f399e8562d922291ff84ba
Sam Salisbury <samsalisbury@gmail.com>
2018-07-18 10:46:11 +0100

    test/smoke: don't shout about skipping pid cleanup

--- test/smoke/testpids_test.go
+++ test/smoke/testpids_test.go
@@ -109,7 +109,9 @@ func stopPIDs() error {
 			return fmt.Errorf("cannot inspect proc %d: %s", pid, err)
 		}
 		if psProc == nil {
-			log.Printf("skipping cleanup of %d (already stopped)", pid)
+			if !quiet() {
+				log.Printf("skipping cleanup of %d (already stopped)", pid)
+			}
 			continue
 		}
 
