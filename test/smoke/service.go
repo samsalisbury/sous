d43203918d5ffa052fee7cd62dc74b32118fab43
Sam Salisbury <samsalisbury@gmail.com>
2018-07-25 11:53:37 +0100

    test/smoke: extract Service type
    - This is the common bits of managing a service under test
      and includes crash detection etc.

---
+++ test/smoke/service.go
@@ -0,0 +1,108 @@
+package smoke
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+// Service represents a binary that is intended to be run as a long-running
+// process. It includes things like crash detection.
+type Service struct {
+	Bin
+	Proc *os.Process
+}
+
+// NewService returns a new service bound to the provided binary.
+func NewService(bin Bin) *Service {
+	return &Service{Bin: bin}
+}
+
+// Start starts this service.
+func (s *Service) Start(t *testing.T, subcmd string, flags Flags, args ...string) {
+	t.Helper()
+
+	prepared := s.Command(t, subcmd, flags, args...)
+
+	cmd := prepared.Cmd
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("error starting server %q: %s", s.InstanceName, err)
+	}
+
+	if cmd.Process == nil {
+		panic("cmd.Process nil after cmd.Start")
+	}
+
+	go func() {
+		id := fmt.Sprintf("%s:%s", t.Name(), s.InstanceName)
+
+		var ps *os.ProcessState
+		select {
+		// In this case the process ended before the test finished.
+		case err := <-func() <-chan error {
+			var err error
+			c := make(chan error, 1)
+			go func() {
+				ps, err = cmd.Process.Wait()
+				c <- err
+			}()
+			return c
+		}():
+			if err != nil {
+				rtLog("SERVER CRASHED: %s: %s; process state: %s", id, err, ps)
+				return
+			}
+			if !ps.Exited() {
+				// NOTE SS: This condition should not be possible, since after
+				// calling Wait, the process should have exited. But it hasn't.
+				rtLog("SERVER DID NOT EXIT: %s", id)
+				return
+			}
+			if ps.Success() {
+				rtLog("SERVER STOPPED: %s (exit code 0)", id)
+			}
+			// TODO SS: Dump log tail here as well for analysis.
+			rtLog("SERVER CRASHED: %s; logs follow:", id)
+			s.DumpTail(t, 50)
+		// In this case the process is still running.
+		case <-s.TestFinished:
+			rtLog("OK: SERVER STILL RUNNING AFTER TEST %s", id)
+			// Do nothing.
+		}
+	}()
+
+	s.Proc = cmd.Process
+	writePID(t, s.Proc.Pid)
+}
+
+// Stop stops this service.
+func (s *Service) Stop() error {
+	if s.Proc == nil {
+		return fmt.Errorf("cannot stop %s (not started)", s.InstanceName)
+	}
+	if err := s.Proc.Kill(); err != nil {
+		return fmt.Errorf("cannot kill %s: %s", s.InstanceName, err)
+	}
+	return nil
+}
+
+// DumpTail prints out the last n lines of combined (stdout + stderr) output
+// from this service.
+func (s *Service) DumpTail(t *testing.T, n int) {
+	path := filepath.Join(s.LogDir, "combined")
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		rtLog("ERROR unable to read log file %s: %s", path, err)
+	}
+	lines := strings.Split(string(b), "\n")
+	out := strings.Join(lines[len(lines)-n:], "\n") + "\n"
+	prefix := fmt.Sprintf("%s:%s:combined> ", t.Name(), s.InstanceName)
+	outPipe, err := prefixedPipe(prefix)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Fprint(outPipe, out)
+}
--- test/smoke/sousserver.go
+++ test/smoke/sousserver.go
@@ -2,11 +2,8 @@ package smoke
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/opentable/sous/config"
@@ -18,11 +15,10 @@ import (
 )
 
 type sousServer struct {
-	Bin
+	*Service
 	Addr                string
 	StateDir, ConfigDir string
 	ClusterName         string
-	Proc                *os.Process
 	// Num is the instance number for display purposes.
 	Num int
 }
@@ -31,17 +27,21 @@ func makeInstance(t *testing.T, binPath string, i int, clusterName, baseDir, add
 	baseDir = path.Join(baseDir, fmt.Sprintf("instance%d", i+1))
 	stateDir := path.Join(baseDir, "state")
 
-	name := fmt.Sprintf("instance%d", i)
+	num := i + 1
+
+	name := fmt.Sprintf("instance%d", num)
 
 	bin := NewBin(binPath, name, baseDir, finished)
 	bin.Env["SOUS_CONFIG_DIR"] = bin.ConfigDir
 
+	service := NewService(bin)
+
 	return &sousServer{
-		Bin:         bin,
+		Service:     service,
 		Addr:        addr,
 		ClusterName: clusterName,
 		StateDir:    stateDir,
-		Num:         i + 1,
+		Num:         num,
 	}, nil
 }
 
@@ -69,7 +69,7 @@ func (i *sousServer) configure(config *config.Config, remoteGDMDir string, fcfg 
 		return err
 	}
 
-	i.Bin.Configure(filemap.FileMap{
+	i.Service.Configure(filemap.FileMap{
 		"config.yaml": string(configYAML),
 	})
 
@@ -96,86 +96,12 @@ func (i *sousServer) Start(t *testing.T) {
 		fmt.Fprintf(os.Stderr, "==> Instance %q config:\n", i.ClusterName)
 	}
 	// Run 'sous config' to validate it.
-	i.Bin.MustRun(t, "config", nil)
+	i.MustRun(t, "config", nil)
 
 	serverDebug := os.Getenv("SOUS_SERVER_DEBUG") == "true"
-	prepared := i.Bin.Command(t, "server", nil, "-listen", i.Addr, "-cluster", i.ClusterName, "autoresolver=false", fmt.Sprintf("-d=%t", serverDebug))
 
-	cmd := prepared.Cmd
-	if err := cmd.Start(); err != nil {
-		t.Fatalf("error starting server %q: %s", i.InstanceName, err)
-	}
+	i.Service.Start(t, "server", nil, "-listen", i.Addr, "-cluster", i.ClusterName, "autoresolver=false", fmt.Sprintf("-d=%t", serverDebug))
 
-	if cmd.Process == nil {
-		panic("cmd.Process nil after cmd.Start")
-	}
-
-	go func() {
-		id := fmt.Sprintf("%s:%s", t.Name(), i.InstanceName)
-
-		var ps *os.ProcessState
-		select {
-		// In this case the process ended before the test finished.
-		case err := <-func() <-chan error {
-			var err error
-			c := make(chan error, 1)
-			go func() {
-				ps, err = cmd.Process.Wait()
-				c <- err
-			}()
-			return c
-		}():
-			if err != nil {
-				rtLog("SERVER CRASHED: %s: %s; process state: %s", id, err, ps)
-				return
-			}
-			if !ps.Exited() {
-				// NOTE SS: This condition should not be possible, since after
-				// calling Wait, the process should have exited. But it hasn't.
-				rtLog("SERVER DID NOT EXIT: %s", id)
-				return
-			}
-			if ps.Success() {
-				rtLog("SERVER STOPPED: %s (exit code 0)", id)
-			}
-			// TODO SS: Dump log tail here as well for analysis.
-			rtLog("SERVER CRASHED: %s; logs follow:", id)
-			i.DumpTail(t, 50)
-		// In this case the process is still running.
-		case <-i.TestFinished:
-			rtLog("OK: SERVER STILL RUNNING AFTER TEST %s", id)
-			// Do nothing.
-		}
-	}()
-
-	i.Proc = cmd.Process
-	writePID(t, i.Proc.Pid)
-}
-
-func (i *sousServer) DumpTail(t *testing.T, n int) {
-	path := filepath.Join(i.LogDir, "combined")
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		rtLog("ERROR unable to read log file %s: %s", path, err)
-	}
-	lines := strings.Split(string(b), "\n")
-	out := strings.Join(lines[len(lines)-n:], "\n") + "\n"
-	prefix := fmt.Sprintf("%s:%s:combined> ", t.Name(), i.InstanceName)
-	outPipe, err := prefixedPipe(prefix)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Fprint(outPipe, out)
-}
-
-func (i *sousServer) Stop() error {
-	if i.Proc == nil {
-		return fmt.Errorf("cannot stop instance %q (not started)", i.Num)
-	}
-	if err := i.Proc.Kill(); err != nil {
-		return fmt.Errorf("cannot kill instance %q: %s", i.Num, err)
-	}
-	return nil
 }
 
 const pidFile = "test-server-pids"
