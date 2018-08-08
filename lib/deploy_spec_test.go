7c169185dca5d14924c9316a979a37a70b4f14d4
Sam Salisbury <samsalisbury@gmail.com>
2018-08-08 12:17:57 +0100

    lib: +test: DeploySpec.Validate

--- lib/deploy_spec_test.go
+++ lib/deploy_spec_test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func TestDeploySpec_ClusterNames(t *testing.T) {
-
 	cases := []struct {
 		in   DeploySpecs
 		want []string
@@ -23,5 +22,29 @@ func TestDeploySpec_ClusterNames(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestDeploySpec_Validate(t *testing.T) {
+	invalidConfig := DeployConfig{}
+	dcFlaws := invalidConfig.Validate()
+	if len(dcFlaws) == 0 {
+		t.Fatalf("test setup failed to produce invalid DeployConfig")
+	}
+
+	invalidSpec := DeploySpec{DeployConfig: invalidConfig}
+	specFlaws := invalidSpec.Validate()
+	if len(specFlaws) < len(dcFlaws) {
+		t.Fatalf("validating DeploySpec produced %d flaws; want at least %d",
+			len(specFlaws), len(dcFlaws))
+	}
+
+	// NOTE SS: Flaws are differentiated by type, assertions on actual value
+	// are difficult.
+	for i, wantFlaw := range dcFlaws {
+		gotFlaw := specFlaws[i]
+		got, want := reflect.TypeOf(gotFlaw), reflect.TypeOf(wantFlaw)
+		if got != want {
+			t.Errorf("flaws do not match, got %d:%s; want %s", i, got, want)
+		}
+	}
 }
