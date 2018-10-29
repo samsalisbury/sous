1e22b37fb4fecd686a8938663e4a4137164a54a2
Sam Salisbury <samsalisbury@gmail.com>
2018-10-29 14:39:14 +0000

    docker: fullRepoName always lowercase
    - Oddly, although these unit tests pass, the smoke test for
      this still fails. Something else is deciding on Docker repo
      name in the smoke tests and in real life.

--- ext/docker/names_test.go
+++ ext/docker/names_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	sous "github.com/opentable/sous/lib"
+	"github.com/opentable/sous/util/logging"
 )
 
 func TestImageRepoName(t *testing.T) {
@@ -14,36 +15,36 @@ func TestImageRepoName(t *testing.T) {
 	}{
 		{
 			sous.SourceLocation{Repo: "github.com/user1/repo1"},
-			"/user1/repo1-docker",
+			"example.org/user1/repo1-docker",
 		},
 		{
 			sous.SourceLocation{Repo: "github.com/User1/Repo1"},
-			"/user1/repo1-docker",
+			"example.org/user1/repo1-docker",
 		},
 		{
 			sous.SourceLocation{Repo: "github.com/opentable/repo1"},
-			"repo1-docker",
+			"example.org/repo1-docker",
 		},
 		{
 			sous.SourceLocation{
 				Repo: "github.com/opentable/repo1",
 				Dir:  "dir1",
 			},
-			"repo1/dir1-docker",
+			"example.org/repo1/dir1-docker",
 		},
 		{
 			sous.SourceLocation{
 				Repo: "github.com/user1/repo1",
 				Dir:  "dir1",
 			},
-			"/user1/repo1/dir1-docker",
+			"example.org/user1/repo1/dir1-docker",
 		},
 	}
 
 	for _, tc := range cases {
 		tc := tc
 		t.Run(tc.want, func(t *testing.T) {
-			got := imageRepoName(tc.in, "docker", stripRE)
+			got := fullRepoName("example.org", tc.in, "docker", stripRE, logging.SilentLogSet())
 			if got != tc.want {
 				t.Errorf("got %s --> %q; want %q", tc.in, got, tc.want)
 			}
