9bb9be315cc61522741909cbbf84947adc1f58ef
Sam Salisbury <samsalisbury@gmail.com>
2018-10-29 15:13:23 +0000

    docker: failing test: uppercase repo blank kind

--- ext/docker/names_test.go
+++ ext/docker/names_test.go
@@ -10,22 +10,28 @@ import (
 func TestImageRepoName(t *testing.T) {
 
 	cases := []struct {
+		kind string
 		in   sous.SourceLocation
 		want string
 	}{
+		// Kind: docker
 		{
+			"docker",
 			sous.SourceLocation{Repo: "github.com/user1/repo1"},
 			"example.org/user1/repo1-docker",
 		},
 		{
+			"docker",
 			sous.SourceLocation{Repo: "github.com/User1/Repo1"},
 			"example.org/user1/repo1-docker",
 		},
 		{
+			"docker",
 			sous.SourceLocation{Repo: "github.com/opentable/repo1"},
 			"example.org/repo1-docker",
 		},
 		{
+			"docker",
 			sous.SourceLocation{
 				Repo: "github.com/opentable/repo1",
 				Dir:  "dir1",
@@ -33,18 +39,52 @@ func TestImageRepoName(t *testing.T) {
 			"example.org/repo1/dir1-docker",
 		},
 		{
+			"docker",
 			sous.SourceLocation{
 				Repo: "github.com/user1/repo1",
 				Dir:  "dir1",
 			},
 			"example.org/user1/repo1/dir1-docker",
 		},
+
+		// With blank kind.
+		{
+			"",
+			sous.SourceLocation{Repo: "github.com/user1/repo1"},
+			"example.org/user1/repo1",
+		},
+		{
+			"",
+			sous.SourceLocation{Repo: "github.com/User1/Repo1"},
+			"example.org/user1/repo1",
+		},
+		{
+			"",
+			sous.SourceLocation{Repo: "github.com/opentable/repo1"},
+			"example.org/repo1",
+		},
+		{
+			"",
+			sous.SourceLocation{
+				Repo: "github.com/opentable/repo1",
+				Dir:  "dir1",
+			},
+			"example.org/repo1/dir1",
+		},
+		{
+			"",
+			sous.SourceLocation{
+				Repo: "github.com/user1/repo1",
+				Dir:  "dir1",
+			},
+			"example.org/user1/repo1/dir1",
+		},
 	}
 
 	for _, tc := range cases {
 		tc := tc
 		t.Run(tc.want, func(t *testing.T) {
-			got := fullRepoName("example.org", tc.in, "docker", stripRE, logging.SilentLogSet())
+			got := fullRepoName("example.org", tc.in, tc.kind, stripRE, logging.SilentLogSet())
 			if got != tc.want {
 				t.Errorf("got %s --> %q; want %q", tc.in, got, tc.want)
 			}
