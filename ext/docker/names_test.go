f9e93126123150e02b7f748745b12758c209f2ae
Sam Salisbury <samsalisbury@gmail.com>
2018-10-30 10:22:27 +0000

    docker: +TestImageRepoName cover existing behavior

--- ext/docker/names_test.go
+++ ext/docker/names_test.go
@@ -92,3 +92,89 @@ func TestFullRepoName(t *testing.T) {
 	}
 
 }
+
+func TestImageRepoName(t *testing.T) {
+
+	cases := []struct {
+		kind string
+		in   sous.SourceLocation
+		want string
+	}{
+		// Kind: docker
+		{
+			"docker",
+			sous.SourceLocation{Repo: "github.com/user1/repo1"},
+			"/user1/repo1-docker",
+		},
+		{
+			"docker",
+			sous.SourceLocation{Repo: "github.com/User1/Repo1"},
+			"/user1/repo1-docker",
+		},
+		{
+			"docker",
+			sous.SourceLocation{Repo: "github.com/opentable/repo1"},
+			"repo1-docker",
+		},
+		{
+			"docker",
+			sous.SourceLocation{
+				Repo: "github.com/opentable/repo1",
+				Dir:  "dir1",
+			},
+			"repo1/dir1-docker",
+		},
+		{
+			"docker",
+			sous.SourceLocation{
+				Repo: "github.com/user1/repo1",
+				Dir:  "dir1",
+			},
+			"/user1/repo1/dir1-docker",
+		},
+
+		// With blank kind.
+		{
+			"",
+			sous.SourceLocation{Repo: "github.com/user1/repo1"},
+			"/user1/repo1",
+		},
+		{
+			"",
+			sous.SourceLocation{Repo: "github.com/User1/Repo1"},
+			"/user1/repo1",
+		},
+		{
+			"",
+			sous.SourceLocation{Repo: "github.com/opentable/repo1"},
+			"repo1",
+		},
+		{
+			"",
+			sous.SourceLocation{
+				Repo: "github.com/opentable/repo1",
+				Dir:  "dir1",
+			},
+			"repo1/dir1",
+		},
+		{
+			"",
+			sous.SourceLocation{
+				Repo: "github.com/user1/repo1",
+				Dir:  "dir1",
+			},
+			"/user1/repo1/dir1",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.kind+"/"+tc.want, func(t *testing.T) {
+			got := imageRepoName(tc.in, tc.kind)
+			if got != tc.want {
+				t.Errorf("got %s --> %q; want %q", tc.in, got, tc.want)
+			}
+		})
+	}
+
+}
