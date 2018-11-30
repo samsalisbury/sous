004b5ddb2b0ec5a7875a533f88c19a748c0be431
Sam Salisbury <samsalisbury@gmail.com>
2018-11-30 10:40:31 +0000

    cli/queries: use sub-tests

--- cli/queries/deployment_filters_test.go
+++ cli/queries/deployment_filters_test.go
@@ -13,6 +13,8 @@ func deploy(sid sous.SourceID) *sous.Deployment {
 	return &sous.Deployment{SourceID: sid}
 }
 
+// assertResultCount asserts that we get want results for the filter in "true"
+// mode, and that we get len(ds) - want results for filter in "false" mode.
 func assertResultCount(t *testing.T, ds sous.Deployments, filter deployFilter, want int) {
 	t.Helper()
 
@@ -41,25 +43,29 @@ func assertResultCount(t *testing.T, ds sous.Deployments, filter deployFilter, w
 }
 
 func TestSimpleFilter(t *testing.T) {
-	ds := sous.NewDeployments(
-		deploy(repoSID("X")),
-		deploy(repoSID("Y")),
-		deploy(repoSID("Z")),
-	)
-	filter := simpleFilter(func(d *sous.Deployment) bool {
-		return d.SourceID == repoSID("X")
+	t.Run("1 result", func(t *testing.T) {
+		ds := sous.NewDeployments(
+			deploy(repoSID("X")),
+			deploy(repoSID("Y")),
+			deploy(repoSID("Z")),
+		)
+		filter := simpleFilter(func(d *sous.Deployment) bool {
+			return d.SourceID == repoSID("X")
+		})
+		assertResultCount(t, ds, filter, 1)
 	})
-	assertResultCount(t, ds, filter, 1)
 }
 
 func TestParallelFilter_ok(t *testing.T) {
-	ds := sous.NewDeployments(
-		deploy(repoSID("X")),
-		deploy(repoSID("Y")),
-		deploy(repoSID("Z")),
-	)
-	filter := parallelFilter(1, func(d *sous.Deployment) (bool, error) {
-		return d.SourceID == repoSID("X"), nil
+	t.Run("1 result", func(t *testing.T) {
+		ds := sous.NewDeployments(
+			deploy(repoSID("X")),
+			deploy(repoSID("Y")),
+			deploy(repoSID("Z")),
+		)
+		filter := parallelFilter(1, func(d *sous.Deployment) (bool, error) {
+			return d.SourceID == repoSID("X"), nil
+		})
+		assertResultCount(t, ds, filter, 1)
 	})
-	assertResultCount(t, ds, filter, 1)
 }
