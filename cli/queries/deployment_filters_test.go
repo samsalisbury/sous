76c4ee20a15387818c897b4ec2a40c4033fed681
Sam Salisbury <samsalisbury@gmail.com>
2018-11-30 10:30:54 +0000

    cli/queries: refactor tests for clarity

--- cli/queries/deployment_filters_test.go
+++ cli/queries/deployment_filters_test.go
@@ -6,14 +6,14 @@ import (
 	sous "github.com/opentable/sous/lib"
 )
 
-func TestSimpleFilter(t *testing.T) {
+func repoSID(repo string) sous.SourceID {
+	return sous.SourceID{Location: sous.SourceLocation{Repo: repo}}
+}
 
+func assertSingleMatchingResult(t *testing.T, filter deployFilter) {
+	t.Helper()
 	deploy := func(sid sous.SourceID) *sous.Deployment {
 		return &sous.Deployment{SourceID: sid}
-	}
-
-	repoSID := func(repo string) sous.SourceID {
-		return sous.SourceID{Location: sous.SourceLocation{Repo: repo}}
 	}
 
 	ds := sous.NewDeployments(
@@ -21,10 +21,6 @@ func TestSimpleFilter(t *testing.T) {
 		deploy(repoSID("Y")),
 		deploy(repoSID("Z")),
 	)
-
-	filter := simpleFilter(func(d *sous.Deployment) bool {
-		return d.SourceID == repoSID("X")
-	})
 
 	trueResult, trueErr := filter(ds, true)
 	falseResult, falseErr := filter(ds, false)
@@ -53,49 +49,16 @@ func TestSimpleFilter(t *testing.T) {
 	}
 }
 
+func TestSimpleFilter(t *testing.T) {
+	filter := simpleFilter(func(d *sous.Deployment) bool {
+		return d.SourceID == repoSID("X")
+	})
+	assertSingleMatchingResult(t, filter)
+}
+
 func TestParallelFilter_ok(t *testing.T) {
-
-	deploy := func(sid sous.SourceID) *sous.Deployment {
-		return &sous.Deployment{SourceID: sid}
-	}
-
-	repoSID := func(repo string) sous.SourceID {
-		return sous.SourceID{Location: sous.SourceLocation{Repo: repo}}
-	}
-
-	ds := sous.NewDeployments(
-		deploy(repoSID("X")),
-		deploy(repoSID("Y")),
-		deploy(repoSID("Z")),
-	)
-
 	filter := parallelFilter(1, func(d *sous.Deployment) (bool, error) {
 		return d.SourceID == repoSID("X"), nil
 	})
-
-	trueResult, trueErr := filter(ds, true)
-	falseResult, falseErr := filter(ds, false)
-
-	if trueErr != nil {
-		t.Fatal(trueErr)
-	}
-	if falseErr != nil {
-		t.Fatal(falseErr)
-	}
-
-	gotTrue := trueResult.Len()
-	gotFalse := falseResult.Len()
-
-	wantTrue := 1
-	wantFalse := 2
-	if wantTrue+wantFalse != ds.Len() {
-		t.Fatalf("bad test: wantTrue + wantFalse != total")
-	}
-
-	if gotTrue != wantTrue {
-		t.Errorf("got %d true results; want %d", gotTrue, wantTrue)
-	}
-	if gotFalse != wantFalse {
-		t.Errorf("got %d false results; want %d", gotFalse, wantFalse)
-	}
+	assertSingleMatchingResult(t, filter)
 }
